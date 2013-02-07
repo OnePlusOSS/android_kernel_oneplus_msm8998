450a879c85586b8908e8031a2dc505b33648d752
Liam Mark <lmark@codeaurora.org>
2013-02-07 14:31:36 -0800

    android/lowmemorykiller: Ignore tasks with freed mm
    A killed task can stay in the task list long after its
    memory has been returned to the system, therefore
    ignore any tasks whose mm struct has been freed.
    
    Change-Id: I76394b203b4ab2312437c839976f0ecb7b6dde4e
    CRs-fixed: 450383
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    Mot-CRs-fixed: (CR)
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -145,6 +145,10 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		if (tsk->flags & PF_KTHREAD)
 			continue;
 
+		/* if task no longer has any memory ignore it */
+		if (test_task_flag(tsk, TIF_MM_RELEASED))
+			continue;
+
 		if (time_before_eq(jiffies, lowmem_deathpending_timeout)) {
 			if (test_task_flag(tsk, TIF_MEMDIE)) {
 				rcu_read_unlock();
@@ -207,11 +211,12 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			     free);
 		lowmem_deathpending_timeout = jiffies + HZ;
 		rem += selected_tasksize;
-	}
+		rcu_read_unlock();
+	} else
+		rcu_read_unlock();
 
 	lowmem_print(4, "lowmem_scan %lu, %x, return %lu\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
-	rcu_read_unlock();
 	return rem;
 }
 

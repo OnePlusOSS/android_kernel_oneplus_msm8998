64f01dec8febd89c3dc2ef2de696be7efb2388cf
arter97 <qkrwngud825@gmail.com>
2014-01-18 20:55:00 +0900

    fs: default to noatime
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2789,9 +2789,9 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 	if (retval)
 		goto dput_out;
 
-	/* Default to relatime unless overriden */
-	if (!(flags & MS_NOATIME))
-		mnt_flags |= MNT_RELATIME;
+	/* Default to noatime unless overriden */
+	if (!(flags & MS_RELATIME))
+		mnt_flags |= MNT_NOATIME;
 
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)

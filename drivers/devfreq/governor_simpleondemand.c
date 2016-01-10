0d9ac6a7e14e572e25e434547aa82ca81ba0de27
andip71 <andreasp@gmx.de>
2016-01-10 12:09:11 +0100

    GPU: governor_simpleondemand tweaked to not peak up when using 27 MHz min frequency

--- drivers/devfreq/governor_simpleondemand.c
+++ drivers/devfreq/governor_simpleondemand.c
@@ -72,10 +72,13 @@ static int devfreq_simple_ondemand_func(struct devfreq *df,
 		return 0;
 	}
 
-	/* Set MAX if it's busy enough */
-	if (stat->busy_time * 100 >
-	    stat->total_time * dfso_upthreshold) {
-		*freq = max;
+	/* Set MAX if it's busy enough - only if current frequency is not below 180 MHz */
+	if (stat.busy_time * 100 >
+		stat.total_time * dfso_upthreshold) {
+		if (stat.current_frequency >= 180000000)
+			*freq = max;
+		else
+			*freq = 180000000;
 		return 0;
 	}
 

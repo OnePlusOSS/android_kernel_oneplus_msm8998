c3f25a2155f6efeb0aa93eb2fbfb8ccf2ab5bc12
John Dias <joaodias@google.com>
2016-09-20 16:34:56 -0700

    sched: fix task-priority when using SCHED_RESET_ON_FORK
    If a call to setscheduler tries to assign a task both RT priority
    and SCHED_RESET_ON_FORK, the scheduler currently sets the
    priority incorrectly, resulting in not only reporting the priority
    incorrectly, but also in scheduling the task using the fair scheduler
    (because the check for RT sees extra bit for RESET_ON_FORK).
    
    Bug: 27705114
    Change-Id: I2eaa137a759cd39e1e98f8d5e1caea046877330d
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4067,6 +4067,8 @@ static void __setscheduler_params(struct task_struct *p,
 
 	if (policy == SETPARAM_POLICY)
 		policy = p->policy;
+	else
+		policy &= ~SCHED_RESET_ON_FORK;
 
 	p->policy = policy;
 

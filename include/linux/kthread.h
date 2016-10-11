4d86053f670718695f41667a610a194fc4882284
Petr Mladek <pmladek@suse.com>
2016-10-11 13:55:43 -0700

    kthread: Backport queuing_blocked()
    This patch backports the queuing_blocked() function
    from Linux mainline and places it into the
    kthread header so it is accessible everywhere.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- include/linux/kthread.h
+++ include/linux/kthread.h
@@ -122,6 +122,18 @@ extern void __init_kthread_worker(struct kthread_worker *worker,
 		(work)->func = (fn);					\
 	} while (0)
 
+/*
+ * Returns true when the work could not be queued at the moment.
+ * It happens when it is already pending in a worker list.
+ */
+static inline bool queuing_blocked(struct kthread_worker *worker,
+				   struct kthread_work *work)
+{
+	lockdep_assert_held(&worker->lock);
+
+	return !list_empty(&work->node);
+}
+
 int kthread_worker_fn(void *worker_ptr);
 
 bool queue_kthread_work(struct kthread_worker *worker,

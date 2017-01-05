27b37c379c08e0afeac4e4914159e7b9b73ecc81
qcabuildsw <qcabuildsw@localhost>
2017-01-05 11:17:01 -0800

    Release 5.1.0.43
    Release 5.1.0.43
    
    Change-Id: I3533b06d9b18f00ed6d89666f4293f19512fd30c
    CRs-Fixed: 688141

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -41,9 +41,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "Z"
-#define QWLAN_VERSION_BUILD            42
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            43
 
-#define QWLAN_VERSIONSTR               "5.1.0.42Z"
+#define QWLAN_VERSIONSTR               "5.1.0.43"
 
 #endif /* QWLAN_VERSION_H */

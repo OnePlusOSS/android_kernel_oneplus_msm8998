8a42648625bbfc5fab743366cb2002e4daaf572d
Ganesh Kondabattini <ganeshk@codeaurora.org>
2017-01-08 20:05:12 +0530

    Revert "qcacld-3.0: Fix to update roc cookie properly"
    This reverts commit Idf8688652447b8b3f528a4e2ade59415a66930f4.
    
    This change is creating mismatch between the cookies of  roc req and
    roc expired event.
    
    CRs-Fixed: 1108767
    Change-Id: Ie248de1a3e78716c34c5dd8575e8fdf80f3c811d

--- core/hdd/src/wlan_hdd_p2p.c
+++ core/hdd/src/wlan_hdd_p2p.c
@@ -1130,7 +1130,7 @@ __wlan_hdd_cfg80211_cancel_remain_on_channel(struct wiphy *wiphy,
 	    (cfgState->remain_on_chan_ctx->cookie != cookie)) {
 		mutex_unlock(&cfgState->remain_on_chan_ctx_lock);
 		hdd_err("No Remain on channel pending with specified cookie value");
-		return 0;
+		return -EINVAL;
 	}
 
 	if (NULL != cfgState->remain_on_chan_ctx) {
@@ -1553,11 +1553,14 @@ send_frame:
 
 		mutex_lock(&cfgState->remain_on_chan_ctx_lock);
 
-		*cookie = (uintptr_t) cfgState->buf;
-		cfgState->action_cookie = *cookie;
-		if (cfgState->remain_on_chan_ctx)
-			cfgState->remain_on_chan_ctx->cookie =
-				cfgState->action_cookie;
+		if (cfgState->remain_on_chan_ctx) {
+			cfgState->action_cookie =
+				cfgState->remain_on_chan_ctx->cookie;
+			*cookie = cfgState->action_cookie;
+		} else {
+			*cookie = (uintptr_t) cfgState->buf;
+			cfgState->action_cookie = *cookie;
+		}
 
 		mutex_unlock(&cfgState->remain_on_chan_ctx_lock);
 	}

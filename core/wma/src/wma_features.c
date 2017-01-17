b70e662383d7497c0a86b2702f4c0feb09c27876
Tang Yingying <yintang@codeaurora.org>
2017-01-17 23:28:43 +0800

    qcacld-3.0: Add "static" before function to fix compilation error
    Some functions are only used in the file which it is defined
    in. Add "static" before function to fix compilation error.
    
    Change-Id: Ie99c8e2a2c4dd6564c19876f8456a47786d3e5cb
    CRs-Fixed: 1112463

--- core/wma/src/wma_features.c
+++ core/wma/src/wma_features.c
@@ -1590,8 +1590,8 @@ int wma_nan_rsp_event_handler(void *handle, uint8_t *event_buf,
 	return 0;
 }
 #else
-int wma_nan_rsp_event_handler(void *handle, uint8_t *event_buf,
-			      uint32_t len)
+static int wma_nan_rsp_event_handler(void *handle, uint8_t *event_buf,
+				     uint32_t len)
 {
 	return 0;
 }

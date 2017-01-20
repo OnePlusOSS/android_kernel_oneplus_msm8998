548ee74e4dc7c325eea33571faa8772ff8df6ee0
Alex Naidis <alex.naidis@linux.com>
2017-01-20 22:13:10 +0100

    cpu-boost: Boost priority of notifier_block
    * Reduces latency of the input boosts.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -154,6 +154,7 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 
 static struct notifier_block boost_adjust_nb = {
 	.notifier_call = boost_adjust_notify,
+	.priority = INT_MAX-2,
 };
 
 static void update_policy_online(void)

99407dc9affe2769a730ed4c7b4da1cadc695450
Manjunathappa Prakash <prakashpm@codeaurora.org>
2017-02-06 20:50:44 -0800

    qcacld-3.0: Disable LRO during SSR procedure
    SSR procedure doesnot call hdd_stop_adapter and recovery procedure calls
    hdd_init_station_mode, leading creation of new LRO manager. This results in
    memory leak on LRO manager data structure. Fix it by calling hdd_disable_lro()
    from hdd_reset_all_adapters(), called during teardown procedure of SSR.
    
    Change-Id: Ib4ec89a2bf784a760ec67eeff7054a8733ca3cb2
    CRs-Fixed: 2003475

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -3811,6 +3811,7 @@ QDF_STATUS hdd_reset_all_adapters(hdd_context_t *hdd_ctx)
 		adapter->sessionCtx.station.hdd_ReassocScenario = false;
 
 		hdd_deinit_tx_rx(adapter);
+		hdd_lro_disable(hdd_ctx, adapter);
 		cds_decr_session_set_pcl(adapter->device_mode,
 						adapter->sessionId);
 		if (test_bit(WMM_INIT_DONE, &adapter->event_flags)) {

d2cb1670646df4c47f03cc78ad07f194bfd6b0fe
Arif Hussain <arifhussain@codeaurora.org>
2017-02-08 14:35:00 -0800

    qcacld-3.0: Refactor code use to search vdev using MAC address
    In LIM, move functionality to find session for a given MAC address in
    separate function so that this code can be reused by other functions
    if needed.
    
    In HDD, remove logic added to find adaptor for a given MAC, instead use
    existing function hdd_get_adapter_by_macaddr.
    
    Change-Id: I989f09ffcbee3a717c22c267a01dafd1b404da64
    CRs-Fixed: 2004223

--- core/mac/src/pe/lim/lim_utils.c
+++ core/mac/src/pe/lim/lim_utils.c
@@ -7308,3 +7308,34 @@ void lim_update_last_processed_frame(last_processed_msg *last_processed_frm,
 	qdf_mem_copy(last_processed_frm->sa, pHdr->sa, ETH_ALEN);
 	last_processed_frm->seq_num = seq_num;
 }
+
+tCsrRoamSession *lim_get_session_by_macaddr(tpAniSirGlobal mac_ctx,
+		tSirMacAddr self_mac)
+{
+	int i = 0;
+	tCsrRoamSession *session;
+
+	if (!mac_ctx || !self_mac) {
+		QDF_TRACE(QDF_MODULE_ID_PE, QDF_TRACE_LEVEL_ERROR,
+			  FL("Invalid arguments"));
+		return NULL;
+	}
+
+	for (i = 0; i < mac_ctx->sme.max_intf_count; i++) {
+		session = CSR_GET_SESSION(mac_ctx, i);
+		if (!session)
+			continue;
+		else if (!qdf_mem_cmp(&session->selfMacAddr,
+			 self_mac, sizeof(tSirMacAddr))) {
+
+			QDF_TRACE(QDF_MODULE_ID_PE, QDF_TRACE_LEVEL_INFO,
+				  FL("session %d exists with mac address "
+				  MAC_ADDRESS_STR), session->sessionId,
+				  MAC_ADDR_ARRAY(self_mac));
+
+			return session;
+		}
+	}
+
+	return NULL;
+}

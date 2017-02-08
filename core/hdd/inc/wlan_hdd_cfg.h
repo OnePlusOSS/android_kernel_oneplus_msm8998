5dedc7212d68cadef66aabe1354beb15e3137af4
Kabilan Kannan <kabilank@codeaurora.org>
2017-02-08 10:29:54 -0800

    qcacld-3.0: Set valid min value for gTDLSPrefOffChanBandwidth
    This is a qcacld-2.0 to qcacld-3.0 propagation.
    
    Currently the min value of gTDLSPrefOffChanBandwidth is set to zero which
    is invalid, min value should be 1 (zeroth bit set) for 20MHz.
    
    Change-Id: Ibc2da1c2cca7e704b21686ab795224462dd9f913
    CRs-Fixed: 2004116

--- core/hdd/inc/wlan_hdd_cfg.h
+++ core/hdd/inc/wlan_hdd_cfg.h
@@ -4746,7 +4746,7 @@ typedef enum {
  * <ini>
  * gTDLSPrefOffChanBandwidth - Preferred TDLS channel bandwidth when
  * off-channel support is enabled.
- * @Min: 0
+ * @Min: 0x01
  * @Max: 0x0F
  * @Default: 0x07
  *
@@ -4765,7 +4765,7 @@ typedef enum {
  * </ini>
  */
 #define CFG_TDLS_PREFERRED_OFF_CHANNEL_BW           "gTDLSPrefOffChanBandwidth"
-#define CFG_TDLS_PREFERRED_OFF_CHANNEL_BW_MIN      (0)
+#define CFG_TDLS_PREFERRED_OFF_CHANNEL_BW_MIN      (0x01)
 #define CFG_TDLS_PREFERRED_OFF_CHANNEL_BW_MAX      (0x0F)
 #define CFG_TDLS_PREFERRED_OFF_CHANNEL_BW_DEFAULT  (0x07)
 

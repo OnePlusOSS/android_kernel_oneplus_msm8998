20454a222cfa91bd31f9e559a7a3bea596e205f5
Kapil Gupta <kapgupta@codeaurora.org>
2017-02-13 15:51:52 +0530

    qcacmn: Add monitor time for PER based roam
    Curretly FW considers minimum time of hysteresis for PER based roam as
    constant value which is 25 seconds.
    
    Add changes to make this time configurable using ini.
    
    Change-Id: Iadb5f6a005b069e0d6190df34e382927425ec1af
    CRs-Fixed: 2005849

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -6659,6 +6659,10 @@ struct wmi_adaptive_dwelltime_params {
  *     will be considered for PER based scan in rx path
  * @per_rest_time: time for which PER based roam will wait once it
  *     issues a roam scan.
+ * @tx_per_mon_time: Minimum time required to be considered as valid scenario
+ *     for PER based roam in tx path
+ * @rx_per_mon_time: Minimum time required to be considered as valid scenario
+ *     for PER based roam in rx path
  */
 struct wmi_per_roam_config {
 	uint32_t enable;
@@ -6669,6 +6673,8 @@ struct wmi_per_roam_config {
 	uint32_t tx_rate_thresh_percnt;
 	uint32_t rx_rate_thresh_percnt;
 	uint32_t per_rest_time;
+	uint32_t tx_per_mon_time;
+	uint32_t rx_per_mon_time;
 };
 
 /**

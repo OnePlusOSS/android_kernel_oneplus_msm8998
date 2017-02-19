2b03a62e2b9d593cc952cd2808fc4063a9e72e5b
Mohit Khanna <mkhannaqca@codeaurora.org>
2017-02-18 18:05:52 -0800

    qcacld-3.0: Improve TX throughput for UDP apps with small buffers
    Currently, in case the application send buffer size is very small (< 64k),
    the application TX throughput is limited by its send buffer
    (sk->sk_sndbuf). Once the send buffer is full (sk->sk_wmem_alloc
    becomes greater or equal to sk->sk_sndbuf), the application can send
    more data only when TX completions have arrived for some of the buffers
    in the send buffer and some space is created.
    
    Allow application(s) to send as much data over the wlan interface as
    they may have, limited by the capability of the driver. Since the
    driver has a limited number of TX descriptors, the maximum amount of
    outstanding TX buffers are limited by the maximum number of TX
    descriptors for that interface. For example, if the number of TX
    descriptors for wlan0 are 1200, there can be at max 1200 (MSDU sized) TX packets.
    
    Change-Id: I75eca2f4e10ecde1a5b347a305e896ac17d4ef7c
    CRs-Fixed: 2016357

--- core/hdd/src/wlan_hdd_wext.c
+++ core/hdd/src/wlan_hdd_wext.c
@@ -2781,7 +2781,7 @@ void hdd_wlan_get_stats(hdd_adapter_t *pAdapter, uint16_t *length,
 
 	len = scnprintf(buffer, buf_len,
 		"\nTransmit[%lu] - "
-		"called %u, dropped %u,"
+		"called %u, dropped %u orphan %u,"
 		"\n[dropped]    BK %u, BE %u, VI %u, VO %u"
 		"\n[classified] BK %u, BE %u, VI %u, VO %u"
 		"\n\nReceive[%lu] - "
@@ -2790,6 +2790,7 @@ void hdd_wlan_get_stats(hdd_adapter_t *pAdapter, uint16_t *length,
 		qdf_system_ticks(),
 		pStats->txXmitCalled,
 		pStats->txXmitDropped,
+		pStats->txXmitOrphaned,
 
 		pStats->txXmitDroppedAC[SME_AC_BK],
 		pStats->txXmitDroppedAC[SME_AC_BE],

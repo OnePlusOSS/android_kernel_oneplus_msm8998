bbf4328ead1b6ce84bd5114cdc16e62ab5652ed1
Agrawal Ashish <ashishka@codeaurora.org>
2017-02-22 14:45:30 +0530

    qcacld-3.0: Move cfg ini based WMI command to hdd_wlan_start_modules
    Move cfg ini based WMI cts2self enable command from hdd_wlan_startup
    to hdd_wlan_start_modules during DRIVER_MODULES_OPENED state.
    After hdd_wlan_startup, host starts the interface timer and if this
    timer gets expired, then driver closes all the modules and cut down
    the power to the chip.
    On iface up, power on happens and modules are started again, and all
    WMI configuration needs to be sent again to FW which happends as part
    of hdd_wlan_start_modules.
    
    Change-Id: I50183b65c88b5b5fec6a09f93dc2c86d39f48039
    CRs-Fixed: 2010097

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -7906,6 +7906,9 @@ static int hdd_features_init(hdd_context_t *hdd_ctx, hdd_adapter_t *adapter)
 	else
 		hdd_set_idle_ps_config(hdd_ctx, false);
 
+	if (hdd_ctx->config->enable_go_cts2self_for_sta)
+	    sme_set_cts2self_for_p2p_go(hdd_ctx->hHal);
+
 	if (hdd_lro_init(hdd_ctx))
 		hdd_warn("Unable to initialize LRO in fw");
 
@@ -8428,9 +8431,6 @@ int hdd_wlan_startup(struct device *dev)
 
 	hdd_release_rtnl_lock();
 	rtnl_held = false;
-
-	if (hdd_ctx->config->enable_go_cts2self_for_sta)
-		sme_set_cts2self_for_p2p_go(hdd_ctx->hHal);
 
 	wlan_hdd_update_11n_mode(hdd_ctx->config);
 

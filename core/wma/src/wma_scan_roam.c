e550092ed4f18c6c9049b86dd99e0c1e897a884f
Arif Hussain <arifhussain@codeaurora.org>
2017-02-22 11:45:50 -0800

    qcacld-3.0: Send WMA_SWITCH_CHANNEL_RSP in wma_set_channel for monitor mode
    In wma_set_channel function for monitor mode WMI_PDEV_SET_CHANNEL_CMDID
    is used to set channel.
    
    In lim, while sending WMA_CHNL_SWITCH_REQ, defer flag was set to wait for
    WMA_SWITCH_CHANNEL_RSP from firmware, incase if we don’t use
    WMA_CHNL_SWITCH_REQ in monitor mode, send WMA_SWITCH_CHANNEL_RSP
    from wma_set_channel itself to reset defer flag in lim.
    
    Change-Id: Ica96be08d8fb2af5ca5dc07182b690ea5c2bdbae
    CRs-Fixed: 2010363

--- core/wma/src/wma_scan_roam.c
+++ core/wma/src/wma_scan_roam.c
@@ -2962,6 +2962,7 @@ void wma_set_channel(tp_wma_handle wma, tpSwitchChannelParams params)
 				 status);
 
 		ol_htt_mon_note_chan(pdev, req.chan);
+		goto send_resp;
 	} else {
 
 		msg = wma_fill_vdev_req(wma, req.vdev_id, WMA_CHNL_SWITCH_REQ,

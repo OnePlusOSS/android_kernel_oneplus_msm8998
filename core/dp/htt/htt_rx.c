3f6e7d1f2ebae0b68adb2bf3e67e763f13fcdaac
Poddar, Siddarth <siddpodd@codeaurora.org>
2017-02-24 15:37:49 +0530

    qcacld-3.0: Enable debug address marking only for 64 bit DMA address system
    Limit the detection of non posted rx buffers from FW only to
    64 bit DMA address system. For systems, where IPA has 32 bit
    DMA address limitation, this debug feature should be disabled.
    
    CRs-Fixed: 2010583
    Change-Id: I9a26d0a423acedb068d99f39929029182f7c6fa1

--- core/dp/htt/htt_rx.c
+++ core/dp/htt/htt_rx.c
@@ -350,7 +350,7 @@ htt_rx_msdu_first_msdu_flag_ll(htt_pdev_handle pdev, void *msdu_desc)
 static qdf_dma_addr_t
 htt_rx_paddr_mark_high_bits(qdf_dma_addr_t paddr)
 {
-#ifdef HELIUMPLUS_PADDR64
+#ifdef ENABLE_DEBUG_ADDRESS_MARKING
 	if (sizeof(qdf_dma_addr_t) > 4) {
 		/* clear high bits, leave lower 37 bits (paddr) */
 		paddr &= 0x01FFFFFFFFF;
@@ -361,7 +361,7 @@ htt_rx_paddr_mark_high_bits(qdf_dma_addr_t paddr)
 	return paddr;
 }
 
-#ifdef HELIUMPLUS_PADDR64
+#ifdef ENABLE_DEBUG_ADDRESS_MARKING
 static qdf_dma_addr_t
 htt_rx_paddr_unmark_high_bits(qdf_dma_addr_t paddr)
 {
@@ -411,7 +411,7 @@ htt_rx_in_ord_paddr_get(uint32_t *u32p)
 {
 	return HTT_RX_IN_ORD_PADDR_IND_PADDR_GET(*u32p);
 }
-#endif /* HELIUMPLUS_PADDR64 */
+#endif /* ENABLE_DEBUG_ADDRESS_MARKING */
 
 /* full_reorder_offload case: this function is called with lock held */
 static int htt_rx_ring_fill_n(struct htt_pdev_t *pdev, int num)

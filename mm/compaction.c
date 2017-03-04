661aaf4d71e5ce82d969bb47e7ffbd3114cd00ea
Alex Naidis <alex.naidis@linux.com>
2017-03-04 15:28:41 +0100

    mm: compaction: Add automatic compaction mechanism
    This adds a simple implementation of a scheduled
    compaction mechanism.
    
    The scheduled compaction gets executed compaction_soff_delay_ms
    (default 3000ms) after the screen gets turned off unless
    the compaction_timeout_ms (default 900000ms) prevents
    it because a memory compaction is not required.
    
    The workqueue is freezable to indicate that this
    shouldn't be running during suspend.
    
    The parameters of the screen off delay and the forced
    timeout are configurable.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -20,6 +20,10 @@
 #include <linux/kthread.h>
 #include <linux/freezer.h>
 #include <linux/page_owner.h>
+#include <linux/fb.h>
+#include <linux/moduleparam.h>
+#include <linux/time.h>
+#include <linux/workqueue.h>
 #include "internal.h"
 
 #ifdef CONFIG_COMPACTION
@@ -1721,6 +1725,47 @@ break_loop:
 	return rc;
 }
 
+static struct workqueue_struct *compaction_wq;
+static struct delayed_work compaction_work;
+static bool screen_on = true;
+static int compaction_timeout_ms = 900000;
+module_param_named(compaction_forced_timeout_ms, compaction_timeout_ms, int,
+			0644);
+static int compaction_soff_delay_ms = 3000;
+module_param_named(compaction_screen_off_delay_ms, compaction_soff_delay_ms, int,
+			0644);
+static unsigned long compaction_forced_timeout;
+
+
+static int fb_notifier_callback(struct notifier_block *self, unsigned long event, void *data)
+{
+	struct fb_event *evdata = data;
+	int *blank;
+
+	if ((event == FB_EVENT_BLANK) && evdata && evdata->data) {
+		blank = evdata->data;
+
+		switch (*blank) {
+		case FB_BLANK_POWERDOWN:
+			screen_on = false;
+			if (time_after(jiffies, compaction_forced_timeout) && !delayed_work_busy(&compaction_work)) {
+				compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
+				queue_delayed_work(compaction_wq, &compaction_work,
+					msecs_to_jiffies(compaction_soff_delay_ms));
+			}
+		break;
+		case FB_BLANK_UNBLANK:
+			screen_on = true;
+		break;
+		}
+	}
+
+	return 0;
+}
+
+static struct notifier_block compaction_notifier_block = {
+	.notifier_call = fb_notifier_callback,
+};
 
 /* Compact all zones within a node */
 static void __compact_pgdat(pg_data_t *pgdat, struct compact_control *cc)
@@ -1797,6 +1842,23 @@ static void compact_nodes(void)
 
 	for_each_online_node(nid)
 		compact_node(nid);
+}
+
+static void do_compaction(struct work_struct *work)
+{
+	/* Return early if the screen is on */
+	if (screen_on)
+		return;
+
+	pr_info("Scheduled memory compaction is starting");
+
+	/* Do full compaction */
+	compact_nodes();
+
+	/* Force compaction timeout */
+	compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
+
+	pr_info("Scheduled memory compaction is completed");
 }
 
 /* The written value is actually unused, all memory is compacted */
@@ -2069,5 +2131,20 @@ static int __init kcompactd_init(void)
 	return 0;
 }
 subsys_initcall(kcompactd_init)
+
+static int  __init scheduled_compaction_init(void)
+{
+	compaction_wq = create_freezable_workqueue("compaction_wq");
+
+	if (!compaction_wq)
+		return -EFAULT;
+
+	INIT_DELAYED_WORK(&compaction_work, do_compaction);
+
+	fb_register_client(&compaction_notifier_block);
+
+	return 0;
+}
+late_initcall(scheduled_compaction_init);
 
 #endif /* CONFIG_COMPACTION */

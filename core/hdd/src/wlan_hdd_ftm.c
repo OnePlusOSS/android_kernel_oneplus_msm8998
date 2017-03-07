b9cd0291948c42d0467e9d7003aa5aeb3434b7e6
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-03-06 16:34:47 -0800

    qcacld-3.0: Reduce the log spam in wlan_hdd_ftm.c
    Move the logs to appropriate log levels to reduce
    the log spam in wlan_hdd_ftm.c
    
    Change-Id: I9e16494bb950779a5617e7bfac3821d1548b7192
    CRs-Fixed: 2014745

--- core/hdd/src/wlan_hdd_ftm.c
+++ core/hdd/src/wlan_hdd_ftm.c
@@ -103,7 +103,6 @@ static uint32_t wlan_ftm_postmsg(uint8_t *cmd_ptr, uint16_t cmd_len)
 	if (QDF_STATUS_SUCCESS != cds_mq_post_message(QDF_MODULE_ID_WMA,
 						      &ftmMsg)) {
 		hdd_err("Failed to post Msg to HAL");
-
 		return QDF_STATUS_E_FAILURE;
 	}
 
@@ -153,7 +152,7 @@ void hdd_ftm_mc_process_msg(void *message)
 	uint32_t data_len;
 
 	if (!message) {
-		hdd_err("Message is NULL, nothing to process.");
+		hdd_debug("Message is NULL, nothing to process");
 		return;
 	}
 

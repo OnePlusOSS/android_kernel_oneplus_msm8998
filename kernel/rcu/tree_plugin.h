2a2ca3f3a99227d82594f9c632e404d565d3267a
John Dias <joaodias@google.com>
2017-03-13 14:58:19 -0700

    rcu: boost: avoid preemption before calling complete()
    Tracing showed that it was possible for a boosted
    thread to be descheduled with its priority de-boosted
    before it had signalled completion to the boosting thread.
    The fix here is to avoid preemption between releasing
    the mutex and calling complete().
    
    Bug: 34193533
    Change-Id: I598adb62ca675e2da5289b397f971127d4090092
    Signed-off-by: John Dias <joaodias@google.com>
    
    The previous commit "rcu: boost: avoid preemption before calling complete()"
    was reverted later from upstream as priority-inversions were happening.
    
    This is an extremely ugly hack to solve the issue.
    Just use this hack instead as it's beneficial for regular Android workloads.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- kernel/rcu/tree_plugin.h
+++ kernel/rcu/tree_plugin.h
@@ -501,10 +501,18 @@ void rcu_read_unlock_special(struct task_struct *t)
 			raw_spin_unlock_irqrestore(&rnp->lock, flags);
 		}
 
-		/* Unboost if we were boosted. */
+		/*
+		 * Unboost if we were boosted.
+		 * Disable preemption to make sure completion is signalled
+		 * without having the task de-scheduled with its priority
+		 * lowered (in which case we're left with no boosted thread
+		 * and possible RCU starvation).
+		 */
 		if (IS_ENABLED(CONFIG_RCU_BOOST) && drop_boost_mutex) {
+			preempt_disable();
 			rt_mutex_unlock(&rnp->boost_mtx);
 			complete(&rnp->boost_completion);
+			preempt_enable();
 		}
 
 		/*

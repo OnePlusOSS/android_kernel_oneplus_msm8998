ef4c9bec188eee694c61701acb1c3570605dc0c8
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-03-25 10:39:23 -0700

    qcacld-3.0: Fix kernel checkpatch warnings in wlan_hdd_ipa.h
    Fix kernel checkpatch warnings in wlan_hdd_ipa.h.
    
    Change-Id: I4a2429957b25981696581e8ea94a21a1bb3e3c67
    CRs-Fixed: 2024274

--- core/hdd/inc/wlan_hdd_ipa.h
+++ core/hdd/inc/wlan_hdd_ipa.h
@@ -173,18 +173,15 @@ static inline void hdd_ipa_uc_stat_query(hdd_context_t *hdd_ctx,
 {
 	*ipa_tx_diff = 0;
 	*ipa_rx_diff = 0;
-	return;
 }
 
 static inline void hdd_ipa_uc_stat_request(hdd_adapter_t *adapter,
 	uint8_t reason)
 {
-	return;
 }
 
 static inline void hdd_ipa_uc_rt_debug_host_dump(hdd_context_t *hdd_ctx)
 {
-	return;
 }
 
 static inline bool hdd_ipa_is_enabled(hdd_context_t *pHddCtx)
@@ -199,7 +196,6 @@ static inline bool hdd_ipa_uc_is_enabled(hdd_context_t *pHddCtx)
 
 static inline void hdd_ipa_dump_info(hdd_context_t *hdd_ctx)
 {
-	return;
 }
 
 static inline int hdd_ipa_uc_ssr_reinit(hdd_context_t *hdd_ctx)
@@ -213,7 +209,6 @@ static inline int hdd_ipa_uc_ssr_deinit(void)
 }
 static inline void hdd_ipa_uc_force_pipe_shutdown(hdd_context_t *hdd_ctx)
 {
-	return;
 }
 
 /**

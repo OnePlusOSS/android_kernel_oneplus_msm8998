3d3e7eab11a815272e7a92d31cb32b8df6d66026
yeshwanth sriram guntuka <ysriramg@codeaurora.org>
2017-03-27 12:51:46 +0530

    qcacld-3.0: Fix memory leak in csr_process_set_hw_mode
    Memory is not freed in csr_process_set_hw_mode in the
    failure case when connection or bt coex is in progress.
    
    Fix is to free memory in failure case.
    
    Change-Id: I6be164c516ce30d5def7b0088c8b62d6409acf6a
    CRs-Fixed: 2024748

--- core/sme/src/csr/csr_api_roam.c
+++ core/sme/src/csr/csr_api_roam.c
@@ -19482,7 +19482,7 @@ QDF_STATUS csr_issue_stored_joinreq(tpAniSirGlobal mac_ctx,
 void csr_process_set_hw_mode(tpAniSirGlobal mac, tSmeCmd *command)
 {
 	uint32_t len;
-	struct s_sir_set_hw_mode *cmd;
+	struct s_sir_set_hw_mode *cmd = NULL;
 	QDF_STATUS status;
 	tSirMsgQ msg;
 	struct sir_set_hw_mode_resp *param;
@@ -19569,6 +19569,8 @@ void csr_process_set_hw_mode(tpAniSirGlobal mac, tSmeCmd *command)
 	}
 	return;
 fail:
+	if (cmd)
+		qdf_mem_free(cmd);
 	param = qdf_mem_malloc(sizeof(*param));
 	if (!param) {
 		sme_err(

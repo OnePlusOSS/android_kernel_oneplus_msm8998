8be9e74130c097aec6f4391082e71db2ffadd720
Arif Hussain <arifhussain@codeaurora.org>
2017-03-29 10:57:27 -0700

    qcacld-3.0: Cleanup in mac_open to avoid freeing p_mac
    In “Change-Id: I3638b576b76c26181cb180d8b37e195ba3b0ecad”
    replaced dynamic allocation of p_mac to used memory from
    .bss segment, So do not free p_mac.
    
    Change-Id: I2aab77871b8b96de65d7732a001934586df635d0
    CRs-Fixed: 2026241

--- core/mac/src/sys/legacy/src/system/src/mac_init_api.c
+++ core/mac/src/sys/legacy/src/system/src/mac_init_api.c
@@ -127,10 +127,8 @@ tSirRetStatus mac_open(tHalHandle *pHalHandle, tHddHandle hHdd,
 			p_mac->gDriverType = eDRIVER_TYPE_MFG;
 
 		/* Call routine to initialize CFG data structures */
-		if (eSIR_SUCCESS != cfg_init(p_mac)) {
-			qdf_mem_free(p_mac);
+		if (eSIR_SUCCESS != cfg_init(p_mac))
 			return eSIR_FAILURE;
-		}
 
 		sys_init_globals(p_mac);
 	}
@@ -140,10 +138,8 @@ tSirRetStatus mac_open(tHalHandle *pHalHandle, tHddHandle hHdd,
 	p_mac->first_scan_done = false;
 
 	status =  pe_open(p_mac, cds_cfg);
-	if (eSIR_SUCCESS != status) {
+	if (eSIR_SUCCESS != status)
 		pe_err("pe_open() failure");
-		qdf_mem_free(p_mac);
-	}
 
 	return status;
 }

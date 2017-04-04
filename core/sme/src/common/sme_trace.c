f19563a04996a608e106349bc90991f34e2b40e2
Nishank Aggarwal <naggar@codeaurora.org>
2017-04-04 14:33:39 +0530

    qcacld-3.0: Fix kernel checkpatch warnings in sme_trace.c
    Fix kernel checkpatch warnings in sme_trace.c
    
    Change-Id: Ib8828dbb510e42f56f6a15542b0adb5e2de17811
    CRs-Fixed: 2025503

--- core/sme/src/common/sme_trace.c
+++ core/sme/src/common/sme_trace.c
@@ -25,14 +25,12 @@
  * to the Linux Foundation.
  */
 
-/************************************************************************
-   smeTrace.c
-
-   \brief implementation for trace related APIs
-
-   \author Kiran Kumar Reddy CH L V
-
-   ========================================================================*/
+/*
+ * DOC: smeTrace.c
+ *  Implementation for trace related APIs
+ *
+ * Author Kiran Kumar Reddy CH L V
+ */
 #include "ani_global.h"          /* for tpAniSirGlobal */
 #include "mac_trace.h"
 #include "sme_trace.h"
@@ -40,7 +38,7 @@
 #ifndef SME_TRACE_RECORD
 void sme_trace_init(tpAniSirGlobal pMac)
 {
-	return;
+
 }
 #endif
 #ifdef SME_TRACE_RECORD
@@ -167,7 +165,6 @@ static uint8_t *sme_trace_get_rx_msg_string(uint32_t code)
 
 	default:
 		return "UNKNOWN";
-		break;
 	}
 }
 
@@ -212,7 +209,6 @@ static uint8_t *sme_trace_get_command_string(uint32_t command)
 		CASE_RETURN_STRING(eSmeCommandNdpDataEndInitiatorRequest);
 	default:
 		return "UNKNOWN";
-		break;
 	}
 }
 
@@ -253,6 +249,7 @@ static void sme_trace_dump(tpAniSirGlobal mac_ctx, tp_qdf_trace_record record,
 
 void sme_trace_init(tpAniSirGlobal pMac)
 {
-	qdf_trace_register(QDF_MODULE_ID_SME, (tp_qdf_trace_cb) &sme_trace_dump);
+	qdf_trace_register(QDF_MODULE_ID_SME, (tp_qdf_trace_cb)
+				&sme_trace_dump);
 }
 #endif

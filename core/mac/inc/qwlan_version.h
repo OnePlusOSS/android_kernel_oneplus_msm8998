a51c9f03fe158efd71f7c6eecb8b5bd9ca71a2ab
Sandeep Puligilla <spuligil@codeaurora.org>
2017-04-15 21:37:47 -0700

    Release 5.1.1.11S
    Release 5.1.1.11S
    
    Change-Id: I5b910179dc6f765a71bdc11cc30c7b12c3727183
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -41,9 +41,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "R"
+#define QWLAN_VERSION_EXTRA            "S"
 #define QWLAN_VERSION_BUILD            11
 
-#define QWLAN_VERSIONSTR               "5.1.1.11R"
+#define QWLAN_VERSIONSTR               "5.1.1.11S"
 
 #endif /* QWLAN_VERSION_H */

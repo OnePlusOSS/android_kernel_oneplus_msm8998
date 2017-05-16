4ab6c07bb96951ae23a960e33937711842257dab
Abhishek Singh <absingh@codeaurora.org>
2017-05-16 12:33:20 +0530

    qcacld-3.0: Dont update RSSI if chan in probe rsp and rx meta mismatch
    If channel in beacon/probe rsp and rx meta mismatch, driver is getting
    the probe resp due to leakage on adjecent channel. In this case the RSSI
    will not be the actual value and thus should not be updated in scan
    cache.
    
    To fix this allow only probe resp frame and drop beacons if channel
    in frame and rx meta mismatch. For probe resp, consider the RSSI
    of the last frame received properly.
    
    Change-Id: I9e1670b6ef1e40201c48fd102d99944b0376bc5e
    CRs-Fixed: 2047737

--- core/mac/inc/ani_global.h
+++ core/mac/inc/ani_global.h
@@ -83,6 +83,9 @@
 #define P2P_WILDCARD_SSID "DIRECT-"     /* TODO Put it in proper place; */
 #define P2P_WILDCARD_SSID_LEN 7
 
+/* Flags for processing single BSS */
+#define WLAN_SKIP_RSSI_UPDATE   0x01
+
 #ifdef WLAN_FEATURE_CONCURRENT_P2P
 #define MAX_NO_OF_P2P_SESSIONS  5
 #endif /* WLAN_FEATURE_CONCURRENT_P2P */

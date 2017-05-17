1cd5b880d71d0c7b85dc3d9f067e932596267b3f
Selvaraj, Sridhar <sselvara@codeaurora.org>
2017-05-17 12:17:03 +0530

    qcacld-3.0: Restrict FILS caps in ext cap to STA mode only
    Currently, host driver sets FILS capability enable if broadcast
    probe response support is enabled(i.e. g_enable_bcast_probe_rsp).
    since this support adds FILS request params IE(which has the max
    channel time STA would wait for AP to respond with broadcast probe
    response) in probe request, FILS capability is set in ext cap.
    However, it is also set in SAP role which leads to scan issues with
    some client stations.
    
    Restrict FILS caps in ext cap to STA role only if
    g_enable_bcast_probe_rsp INI is set.
    
    Change-Id: Ia5fc0cbf36a4a6df3eb21015ef0508f6daa7b21d
    CRs-Fixed: 2048318

--- core/sme/src/csr/csr_api_roam.c
+++ core/sme/src/csr/csr_api_roam.c
@@ -15226,6 +15226,11 @@ QDF_STATUS csr_send_join_req_msg(tpAniSirGlobal pMac, uint32_t sessionId,
 		}
 		csr_join_req->ignore_assoc_disallowed =
 					pSession->ignore_assoc_disallowed;
+
+		if (pSession->pCurRoamProfile->csrPersona == QDF_STA_MODE)
+			csr_join_req->enable_bcast_probe_rsp =
+				pMac->roam.configParam.enable_bcast_probe_rsp;
+
 		status = cds_send_mb_message_to_mac(csr_join_req);
 		if (!QDF_IS_STATUS_SUCCESS(status)) {
 			/*

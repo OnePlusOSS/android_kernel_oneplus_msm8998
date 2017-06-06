584fb1f9ec2f005b524042f55f3c84929e02219c
Rajeev Kumar Sirasanagandla <rsirasan@codeaurora.org>
2017-06-06 13:11:17 +0530

    qcacld-3.0: Send proper CTL info to fw in world regd
    Currently, for world regd and some other regds where conformance test
    limit is NO_CTL, host is sending invalid CTL info to fw instead of
    NO_CTL (0xff) due to type conversion issues.
    
    To fix this, use proper unsigned type for CTL info.
    
    Change-Id: I6aff04f0142b5c8e4ff2ea91d85ae7108390c89f
    CRs-Fixed: 2056834

--- core/wma/src/wma_features.c
+++ core/wma/src/wma_features.c
@@ -7718,8 +7718,8 @@ QDF_STATUS wma_set_reg_domain(void *clientCtxt, v_REGDOMAIN_t regId)
  * Return: none
  */
 void wma_send_regdomain_info_to_fw(uint32_t reg_dmn, uint16_t regdmn2G,
-				   uint16_t regdmn5G, int8_t ctl2G,
-				   int8_t ctl5G)
+				   uint16_t regdmn5G, uint8_t ctl2G,
+				   uint8_t ctl5G)
 {
 	tp_wma_handle wma = cds_get_context(QDF_MODULE_ID_WMA);
 	int32_t cck_mask_val = 0;

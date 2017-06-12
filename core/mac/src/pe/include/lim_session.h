1c758fa45ff3a66b5c3a2812b2ff068129e94a3f
Tushnim Bhattacharyya <tushnimb@codeaurora.org>
2017-06-12 12:51:30 -0700

    qcacld-3.0: Modify logic to associate with HT clients
    Add the following rules when HT client is associating with SAP,
    1.2G-AS platform: SAP associates with HT (11n) clients as 2x1 in 2G
    and 2X2 in 5G.
    2.Non-2G-AS platform: SAP associates with HT (11n) clients as 2X2
    in 2G and 5G and async DBS scan is disabled.
    3.5G-AS platform: SAP associates with HT (11n) clients as 2X2 in 2G
    and 5G and async DBS scan is disabled.
    
    Change-Id: Ia472e786cf3ffcb420aa8142a543b56f508c6b4a
    CRs-Fixed: 2061436

--- core/mac/src/pe/include/lim_session.h
+++ core/mac/src/pe/include/lim_session.h
@@ -501,6 +501,7 @@ typedef struct sPESession       /* Added to Support BT-AMP */
 	uint8_t deauthmsgcnt;
 	uint8_t disassocmsgcnt;
 	bool enable_bcast_probe_rsp;
+	uint8_t ht_client_cnt;
 } tPESession, *tpPESession;
 
 /*-------------------------------------------------------------------------

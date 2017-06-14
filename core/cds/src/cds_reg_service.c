c09d8360fea4b794c85c4bc93a3fdd580ea04503
Zhu Jianmin <jianminz@codeaurora.org>
2017-06-14 17:54:16 +0800

    qcacld-3.0: Prevent memory issue with more check
    Change proberesp_ies_len to beacon->proberesp_ies_len + MAX_GENIE_LEN
    Add session_id range check
    Check return value of cds_get_channel_enum
    
    Change-Id: I749076a5f060873702b42ac4908e3220e805c349
    CRs-Fixed: 2061117

--- core/cds/src/cds_reg_service.c
+++ core/cds/src/cds_reg_service.c
@@ -357,6 +357,8 @@ enum channel_state cds_get_2g_bonded_channel_state(uint16_t oper_ch,
 		return chan_state;
 
 	chan_enum = cds_get_channel_enum(oper_ch);
+	if (INVALID_CHANNEL == chan_enum)
+		return CHANNEL_STATE_INVALID;
 	if (CH_WIDTH_5MHZ == ch_width)
 		bw_enabled = true;
 	else if (CH_WIDTH_10MHZ == ch_width)

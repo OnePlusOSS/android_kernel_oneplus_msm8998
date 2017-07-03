28b1c368deea8fe0e6aa73546f7b72d621ee3957
Adin Kwok <adin.kwok@carbonrom.org>
2017-07-02 19:03:17 -0700

    sched: Disable gentle fair sleepers

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -3,7 +3,7 @@
  * them to run sooner, but does not allow tons of sleepers to
  * rip the spread apart.
  */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, true)
+SCHED_FEAT(GENTLE_FAIR_SLEEPERS, false)
 
 /*
  * Place new tasks ahead so that they do not starve already running

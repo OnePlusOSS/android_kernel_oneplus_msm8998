2f483c49bf39a8d6eb874e953a3de548072555e0
Liangwei Dong <liangwei@codeaurora.org>
2017-07-05 02:00:01 -0400

    qcacld-3.0: skip FW dump for SDIO SSR
    Skip FW memory dump in SSR case for HL SDIO solution.
    HL SDIO uses the diag read API to read by 4 bytes,
    which is too slow to meet SSR time requirement.
    
    Change-Id: Iacde90ab0ec84f29f3744bf4497ec8fd8517600a
    CRs-Fixed: 2071329

--- core/pld/inc/pld_common.h
+++ core/pld/inc/pld_common.h
@@ -528,4 +528,5 @@ int pld_smmu_map(struct device *dev, phys_addr_t paddr,
 unsigned int pld_socinfo_get_serial_number(struct device *dev);
 int pld_is_qmi_disable(struct device *dev);
 int pld_force_assert_target(struct device *dev);
+bool pld_is_fw_dump_skipped(struct device *dev);
 #endif

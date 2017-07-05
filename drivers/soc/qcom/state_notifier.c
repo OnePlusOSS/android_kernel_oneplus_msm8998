cc36d1de1f76de8e4db88b167e41d66af5f3b646
frap129 <joe@frap129.org>
2017-07-05 22:13:34 +0100

    state_notifier: Queue work on any core
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -2,6 +2,7 @@
  * State Notifier Driver
  *
  * Copyright (c) 2013-2016, Pranav Vashi <neobuddy89@gmail.com>
+ *           (c) 2017, Joe Maples <joe@frap129.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -96,7 +97,8 @@ void state_suspend(void)
 
 	suspend_in_progress = true;
 
-	queue_delayed_work_on(0, susp_wq, &suspend_work, 
+	INIT_DELAYED_WORK(&suspend_work, _suspend_work);
+	queue_delayed_work(susp_wq, &suspend_work,
 		msecs_to_jiffies(suspend_defer_time * 1000));
 }
 
@@ -107,7 +109,7 @@ void state_resume(void)
 	suspend_in_progress = false;
 
 	if (state_suspended)
-		queue_work_on(0, susp_wq, &resume_work);
+		queue_work(susp_wq, &resume_work);
 }
 
 static int __init state_notifier_init(void)

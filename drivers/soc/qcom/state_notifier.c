6e256dcb7906f1ad9fcc233a3a9c2ff443ca021b
frap129 <joe@frap129.org>
2017-07-05 22:18:32 +0100

    state_notifier: Reduce defer on suspend call to 1 second
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -14,7 +14,8 @@
 #include <linux/module.h>
 #include <linux/state_notifier.h>
 
-#define DEFAULT_SUSPEND_DEFER_TIME 	10
+#define DEFAULT_SUSPEND_DEFER_TIME 	1
+#define DEFAULT_USE_FB_NOTIFIER 	0
 #define STATE_NOTIFIER			"state_notifier"
 
 /*

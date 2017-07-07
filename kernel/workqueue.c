719687ba9a91203066f6c07c39fff8db78ce3e65
matthewdalex <matthewalex@linux.com>
2017-07-06 21:37:10 -0400

    workqueue: change permissions to allow root control of wq_power_efficient toggle

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -290,7 +290,7 @@ module_param_named(disable_numa, wq_disable_numa, bool, 0444);
 
 /* see the comment above the definition of WQ_POWER_EFFICIENT */
 static bool wq_power_efficient = IS_ENABLED(CONFIG_WQ_POWER_EFFICIENT_DEFAULT);
-module_param_named(power_efficient, wq_power_efficient, bool, 0444);
+module_param_named(power_efficient, wq_power_efficient, bool, 0644);
 
 static bool wq_numa_enabled;		/* unbound NUMA affinity enabled */
 

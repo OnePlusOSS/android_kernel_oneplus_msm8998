aa9eb3095a5b9d25ac1229007f4ce49e80f0597a
Francisco Franco <franciscofranco.1990@gmail.com>
2017-07-11 11:24:24 -0700

    drivers: power: bcl: queue work on system_power_efficient_wq
    There doesn't seem to be any real dependency of scheduling these on
    the cpu which scheduled them, so moving every *_delayed to the
    power efficient wq save potential needlessly idle cpu wake ups
    leaving the scheduler to decide the most appropriate cpus to wake up.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/power/supply/qcom/bcl_peripheral.c
+++ drivers/power/supply/qcom/bcl_peripheral.c
@@ -799,7 +799,8 @@ exit_ibat:
 
 reschedule_ibat:
 	mutex_unlock(&perph_data->state_trans_lock);
-	schedule_delayed_work(&perph_data->poll_work,
+		queue_delayed_work(system_power_efficient_wq,
+		        &perph_data->poll_work,
 		msecs_to_jiffies(perph_data->polling_delay_ms));
 	trace_bcl_hw_event("ibat poll low. Exit");
 	return;
@@ -845,7 +846,8 @@ exit_vbat:
 
 reschedule_vbat:
 	mutex_unlock(&perph_data->state_trans_lock);
-	schedule_delayed_work(&perph_data->poll_work,
+		queue_delayed_work(system_power_efficient_wq,
+		        &perph_data->poll_work,
 		msecs_to_jiffies(perph_data->polling_delay_ms));
 	trace_bcl_hw_event("vbat poll high. Exit");
 	return;
@@ -888,7 +890,8 @@ static irqreturn_t bcl_handle_ibat(int irq, void *data)
 				perph_data->trip_val);
 		perph_data->ops.notify(perph_data->param_data,
 			perph_data->trip_val, BCL_HIGH_TRIP);
-		schedule_delayed_work(&perph_data->poll_work,
+			queue_delayed_work(system_power_efficient_wq,
+		                &perph_data->poll_work,
 			msecs_to_jiffies(perph_data->polling_delay_ms));
 	} else {
 		pr_debug("Ignoring interrupt\n");
@@ -935,7 +938,8 @@ static irqreturn_t bcl_handle_vbat(int irq, void *data)
 				perph_data->trip_val);
 		perph_data->ops.notify(perph_data->param_data,
 			perph_data->trip_val, BCL_LOW_TRIP);
-		schedule_delayed_work(&perph_data->poll_work,
+			queue_delayed_work(system_power_efficient_wq,
+		                &perph_data->poll_work,
 			msecs_to_jiffies(perph_data->polling_delay_ms));
 	} else {
 		pr_debug("Ignoring interrupt\n");

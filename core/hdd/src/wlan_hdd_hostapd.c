2df35a45a92ccaa7a4fa9bd0c08cdac4bad518b8
Tushnim Bhattacharyya <tushnimb@codeaurora.org>
2017-07-12 13:21:58 -0700

    qcacld-3.0: Do not remove the 5G channel from the favorite channel list
    Do not remove the 5G channel from the favorite channel list when SAP
    is turned off. Leave it in the list till driver unloads.
    
    Change-Id: If009fbfa1d43e80e61634e423ac7ebcc5412a0f9
    CRs-Fixed: 2073175

--- core/hdd/src/wlan_hdd_hostapd.c
+++ core/hdd/src/wlan_hdd_hostapd.c
@@ -8113,11 +8113,6 @@ static int __wlan_hdd_cfg80211_stop_ap(struct wiphy *wiphy,
 	wlan_hdd_undo_acs(pAdapter);
 	qdf_mem_zero(&pConfig->acs_cfg, sizeof(struct sap_acs_cfg));
 
-	/* Remove the channel no from sap mandatory list if it is a
-	 * 5GHz channel */
-	if (CDS_IS_CHANNEL_5GHZ(pConfig->channel))
-		cds_remove_sap_mandatory_chan(pConfig->channel);
-
 	/* Stop all tx queues */
 	hdd_debug("Disabling queues");
 	wlan_hdd_netif_queue_control(pAdapter,

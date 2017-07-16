9961aa43279e56316b8ae827f087acec64b5b9b7
Alex Naidis <alex.naidis@linux.com>
2017-07-16 18:37:11 +0200

    mm: compaction: Fix bad logging
    We need to add \n to start a new line.
    
    Change-Id: Icf60fbd4d723603d4fc1229f5b5a7c232e85ba74
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1850,7 +1850,7 @@ static void do_compaction(struct work_struct *work)
 	if (screen_on)
 		return;
 
-	pr_info("Scheduled memory compaction is starting");
+	pr_info("Scheduled memory compaction is starting\n");
 
 	/* Do full compaction */
 	compact_nodes();
@@ -1858,7 +1858,7 @@ static void do_compaction(struct work_struct *work)
 	/* Force compaction timeout */
 	compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
 
-	pr_info("Scheduled memory compaction is completed");
+	pr_info("Scheduled memory compaction is completed\n");
 }
 
 /* The written value is actually unused, all memory is compacted */

19a5892ec39af1772dd5d36b7b51cddffae1d44c
Poddar, Siddarth <siddpodd@codeaurora.org>
2017-07-17 16:28:03 +0530

    qcacld-3.0: Pass hif_ctx instead of using cds_get_context()
    Return value from cds_get_context() may be NULL and will be dereferenced.
    Instead use already derived hif_ctx in the function.
    
    CRs-Fixed: 2077567
    Change-Id: I6e396b478989768c2b9dd43010fe21749a412074

--- core/hdd/src/wlan_hdd_driver_ops.c
+++ core/hdd/src/wlan_hdd_driver_ops.c
@@ -266,9 +266,9 @@ int hdd_hif_open(struct device *dev, void *bdev, const struct hif_bus_id *bid,
 		}
 	}
 
-	hif_set_ce_service_max_yield_time(cds_get_context(QDF_MODULE_ID_HIF),
+	hif_set_ce_service_max_yield_time(hif_ctx,
 				hdd_ctx->config->ce_service_max_yield_time);
-	hif_set_ce_service_max_rx_ind_flush(cds_get_context(QDF_MODULE_ID_HIF),
+	hif_set_ce_service_max_rx_ind_flush(hif_ctx,
 				hdd_ctx->config->ce_service_max_rx_ind_flush);
 
 	return 0;

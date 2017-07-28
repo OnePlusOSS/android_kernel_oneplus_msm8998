c0937172c6fe590afa588645b0393f2262706d0a
Orhan K AKYILDIZ <oka@codeaurora.org>
2017-07-28 14:45:10 -0700

    qcacld-3.0: Trigger recovery in rx-hash no-match
    In case host receives a paddr in a HTT_RX_IN_ORDER_IND_PADDR msg
    from FW that is not found in the rx_hash, instead of forcing an
    APPS crash, iniate a host-triggered recovery. If the feature
    is not enabled, this would be equivalent to the old code.
    
    Change-Id: Iaab5329c13d626c4b94c690f5537899eecf2637b
    CRs-Fixed: 2084522

--- core/dp/htt/htt_rx.c
+++ core/dp/htt/htt_rx.c
@@ -3334,7 +3334,10 @@ qdf_nbuf_t htt_rx_hash_list_lookup(struct htt_pdev_t *pdev,
 	if (netbuf == NULL) {
 		qdf_print("rx hash: %s: no entry found for %p!\n",
 			  __func__, (void *)paddr);
-		HTT_ASSERT_ALWAYS(0);
+		if (cds_is_self_recovery_enabled())
+			cds_trigger_recovery();
+		else
+			HTT_ASSERT_ALWAYS(0);
 	}
 
 	return netbuf;

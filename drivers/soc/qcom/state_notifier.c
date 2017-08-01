b455b24b860d9b1763d9b30d0967a15e70b47aed
jancsessz <jancsessz@gmail.com>
2017-08-01 19:36:10 +0200

    Revert "state_notifier: Queue work on any core"
    This reverts commit cc36d1de1f76de8e4db88b167e41d66af5f3b646.

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -2,7 +2,6 @@
  * State Notifier Driver
  *
  * Copyright (c) 2013-2016, Pranav Vashi <neobuddy89@gmail.com>
- *           (c) 2017, Joe Maples <joe@frap129.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -97,8 +96,7 @@ void state_suspend(void)
 
 	suspend_in_progress = true;
 
-	INIT_DELAYED_WORK(&suspend_work, _suspend_work);
-	queue_delayed_work(susp_wq, &suspend_work,
+	queue_delayed_work_on(0, susp_wq, &suspend_work, 
 		msecs_to_jiffies(suspend_defer_time * 1000));
 }
 
@@ -109,7 +107,7 @@ void state_resume(void)
 	suspend_in_progress = false;
 
 	if (state_suspended)
-		queue_work(susp_wq, &resume_work);
+		queue_work_on(0, susp_wq, &resume_work);
 }
 
 static int __init state_notifier_init(void)

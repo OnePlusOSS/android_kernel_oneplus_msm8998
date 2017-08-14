f077f00f48235d25734868e5337c8639018c6f34
Alexander Martinz <eviscerationls@gmail.com>
2017-08-14 09:39:26 +0200

    include: asm-generic: do not hard code HZ to 100
    Use the defconfig value instead
    
    Change-Id: I529d08cf06d64f1e3976c0cd876f40b7efefa222
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- include/asm-generic/param.h
+++ include/asm-generic/param.h
@@ -5,6 +5,6 @@
 
 # undef HZ
 # define HZ		CONFIG_HZ	/* Internal kernel timer frequency */
-# define USER_HZ	100		/* some user interfaces are */
+# define USER_HZ	CONFIG_HZ	/* some user interfaces are */
 # define CLOCKS_PER_SEC	(USER_HZ)       /* in "ticks" like times() */
 #endif /* __ASM_GENERIC_PARAM_H */

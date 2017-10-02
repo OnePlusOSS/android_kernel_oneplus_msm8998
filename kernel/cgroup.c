3abe8bdb69bacf3b28754a063492a33a3218333c
liwei <liwei@oneplus.net>
2017-10-02 16:17:39 +0800

    Improve stability
    Use create_workqueue instead of alloc_workqueue.
    
    Change-Id: Ice6aaa9b4b64f340cad53b4915dc22887895e33b

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -5417,7 +5417,7 @@ static int __init cgroup_wq_init(void)
 	 * We would prefer to do this in cgroup_init() above, but that
 	 * is called before init_workqueues(): so leave this until after.
 	 */
-	cgroup_destroy_wq = alloc_workqueue("cgroup_destroy", 0, 1);
+	cgroup_destroy_wq = create_workqueue("cgroup_destroy");
 	BUG_ON(!cgroup_destroy_wq);
 
 	/*

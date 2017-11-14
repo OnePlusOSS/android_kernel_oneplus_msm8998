2610429e20e1481279800663a228626c46ed4ba4
Amar Singhal <asinghal@codeaurora.org>
2017-11-14 13:38:08 -0800

    qcacld-3.0: Regdomain changes for ETSI13
    ETSI13 supports the new EU standard EN 300 440. Therefore move
    the countries that support this standard to this new
    regulatory domain.
    
    Change-Id: I1cebe3c0ed4f071ed5563c40ff56b939303a8d0b
    CRs-Fixed: 2144104

--- core/cds/inc/cds_regdomain.h
+++ core/cds/inc/cds_regdomain.h
@@ -310,6 +310,7 @@ enum reg_domain {
 	ETSI9_WORLD = 0x3E,
 	ETSI10_WORLD = 0x24,
 	ETSI11_WORLD = 0x26,
+	ETSI13_WORLD = 0x28,
 
 	APL4_WORLD = 0x42,
 	APL3_FCCA = 0x50,
@@ -412,6 +413,7 @@ enum reg_domain {
 	ETSI9 = 0x0930,
 	ETSI10 = 0x0D30,
 	ETSI11 = 0x0E30,
+	ETSI13 = 0x0E39,
 
 	APL1 = 0x0150,
 	APL2 = 0x0250,

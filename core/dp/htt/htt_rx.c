b0c8f25d29f32991bf53e82ba87b6d959c407d56
Tiger Yu <tfyu@codeaurora.org>
2017-12-08 15:53:40 +0800

    qcacld-3.0: Fix potential BUG_ON in the htt_rx_offload_msdu_pop_ll
    qcacld-2.0 to qcacld-3.0 propagation
    
    For HTT_T2H_MSG_TYPE_RX_OFFLOAD_DELIVER_IND, the msdu_cnt is a signed
    integer coming from firmware. If set the msdu_cnt to a negative value,
    or be greater than the number of current elements in the queue, the loop
    will execute lots of times in ol_rx_offload_deliver_ind_handler, the
    htt_rx_netbuf_pop will cause the BUG_ON issue sooner or later if it is
    low latency solution.
    
    Change the msdu_cnt type from signed to unsigned and add the validity
    msdu_cnt checking will fix this issue.
    
    Change-Id: I436557a124074f59ab11fd937dfdc975b9caebe8
    CRs-Fixed: 2149461

--- core/dp/htt/htt_rx.c
+++ core/dp/htt/htt_rx.c
@@ -1579,6 +1579,13 @@ htt_rx_frag_pop_hl(
 }
 
 static inline int
+htt_rx_offload_msdu_cnt_hl(
+    htt_pdev_handle pdev)
+{
+    return 1;
+}
+
+static inline int
 htt_rx_offload_msdu_pop_hl(htt_pdev_handle pdev,
 			   qdf_nbuf_t offload_deliver_msg,
 			   int *vdev_id,
@@ -1625,6 +1632,13 @@ htt_rx_offload_msdu_pop_hl(htt_pdev_handle pdev,
 #endif
 
 #ifndef CONFIG_HL_SUPPORT
+static inline int
+htt_rx_offload_msdu_cnt_ll(
+    htt_pdev_handle pdev)
+{
+    return htt_rx_ring_elems(pdev);
+}
+
 static int
 htt_rx_offload_msdu_pop_ll(htt_pdev_handle pdev,
 			   qdf_nbuf_t offload_deliver_msg,
@@ -2995,6 +3009,10 @@ int (*htt_rx_frag_pop)(htt_pdev_handle pdev,
 		       uint32_t *msdu_count);
 
 int
+(*htt_rx_offload_msdu_cnt)(
+    htt_pdev_handle pdev);
+
+int
 (*htt_rx_offload_msdu_pop)(htt_pdev_handle pdev,
 			   qdf_nbuf_t offload_deliver_msg,
 			   int *vdev_id,
@@ -3671,6 +3689,7 @@ int htt_rx_attach(struct htt_pdev_t *pdev)
 	pdev->rx_ring.base_paddr = 0;
 	htt_rx_amsdu_pop = htt_rx_amsdu_pop_hl;
 	htt_rx_frag_pop = htt_rx_frag_pop_hl;
+	htt_rx_offload_msdu_cnt = htt_rx_offload_msdu_cnt_hl;
 	htt_rx_offload_msdu_pop = htt_rx_offload_msdu_pop_hl;
 	htt_rx_mpdu_desc_list_next = htt_rx_mpdu_desc_list_next_hl;
 	htt_rx_mpdu_desc_retry = htt_rx_mpdu_desc_retry_hl;
@@ -3816,6 +3835,7 @@ int htt_rx_attach(struct htt_pdev_t *pdev)
 	if (cds_get_conparam() == QDF_GLOBAL_MONITOR_MODE)
 		htt_rx_amsdu_pop = htt_rx_mon_amsdu_rx_in_order_pop_ll;
 
+	htt_rx_offload_msdu_cnt = htt_rx_offload_msdu_cnt_ll;
 	htt_rx_offload_msdu_pop = htt_rx_offload_msdu_pop_ll;
 	htt_rx_mpdu_desc_retry = htt_rx_mpdu_desc_retry_ll;
 	htt_rx_mpdu_desc_seq_num = htt_rx_mpdu_desc_seq_num_ll;

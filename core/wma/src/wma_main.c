ee28829062ffcc2a414943054fc3735cb082d702
Vignesh Viswanathan <viswanat@codeaurora.org>
2017-12-12 14:42:45 +0530

    qcacld-3.0: Add missing free and break in wma_mc_process_msg
    Add missing free and break in wma_mc_process_msg while handling
    SIR_HAL_CONFIG_GUARD_TIME message in WMA.
    
    Change-Id: I5f4481c937d5c370b334f2a7f8a172d08140ab1d
    CRs-Fixed: 2154304

--- core/wma/src/wma_main.c
+++ core/wma/src/wma_main.c
@@ -7900,6 +7900,8 @@ QDF_STATUS wma_mc_process_msg(void *cds_context, cds_msg_t *msg)
 		wma_config_guard_time(wma_handle,
 				      (struct sir_guard_time_request *)
 				      msg->bodyptr);
+		qdf_mem_free(msg->bodyptr);
+		break;
 	case WMA_IPA_OFFLOAD_ENABLE_DISABLE:
 		wma_ipa_offload_enable_disable(wma_handle,
 			(struct sir_ipa_offload_enable_disable *)msg->bodyptr);

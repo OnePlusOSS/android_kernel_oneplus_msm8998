391f93037b8e9e2694d8e4c1ef1bc3cbc5f44627
gaurank kathpalia <gkathpal@codeaurora.org>
2017-12-14 21:46:23 +0530

    qcacld-3.0: Fix SAP replying to BC probe request in hiddenssid
    The SAP configured in hidden ssid mode sends probe response
    to BC probe requests in hidden ssid mode after force SCC.
    In wma_set_channel, req.ssidhidden is not set by the host
    which in turn sets the param->flags last bit to 0, and
    the firmware receives  these flags and sends the probe
    responses.In the wma_vdev_start api, the host sets the flags
    based upon the req.ssidhidden received from wma_set_channel
    only in vdev start case , and not in vdev restart case.
    
    Fix is to set the value of req.ssidhidden in wma_set_channel
    and to set the hidden ssid flag in both vdev start and vdev
    restart case.
    
    Change-Id: I988d8d64b06128a37824c7d31d4407247ba46dce
    CRs-Fixed: 2142430

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -2552,11 +2552,8 @@ QDF_STATUS wma_vdev_start(tp_wma_handle wma,
 				     temp_ssid_len);
 		}
 
-		params.hidden_ssid = req->hidden_ssid;
 		params.pmf_enabled = req->pmf_enabled;
 		params.ldpc_rx_enabled = req->ldpc_rx_enabled;
-		if (req->hidden_ssid)
-			temp_flags |= WMI_UNIFIED_VDEV_START_HIDDEN_SSID;
 
 		if (req->pmf_enabled)
 			temp_flags |= WMI_UNIFIED_VDEV_START_PMF_ENABLED;
@@ -2564,6 +2561,9 @@ QDF_STATUS wma_vdev_start(tp_wma_handle wma,
 		if (req->ldpc_rx_enabled)
 			temp_flags |= WMI_UNIFIED_VDEV_START_LDPC_RX_ENABLED;
 	}
+	params.hidden_ssid = req->hidden_ssid;
+	if (req->hidden_ssid)
+		temp_flags |= WMI_UNIFIED_VDEV_START_HIDDEN_SSID;
 
 	params.num_noa_descriptors = 0;
 	params.preferred_rx_streams = req->preferred_rx_streams;

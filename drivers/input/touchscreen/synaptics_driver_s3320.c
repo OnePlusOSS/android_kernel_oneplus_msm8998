a0a00261bd5b2845181958d839bc4c33e7143798
Park Ju Hyung <qkrwngud825@gmail.com>
2017-12-28 00:37:01 +0900

    s3320: fix gestures not working
    WTH OnePlus?
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1217,6 +1217,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 		    (gesture_buffer[2] == 0x73) ? Sgestrue : UnkownGestrue;
 	}
 
+#if 0
 	keyCode = UnkownGestrue;
 	/* Get key code based on registered gesture */
 	switch (gesture) {
@@ -1253,6 +1254,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 	default:
 		break;
 	}
+#endif
 
 	TPD_ERR("detect %s gesture\n", gesture == DouTap ? "(double tap)" :
 		gesture == UpVee ? "(V)" :

736abc689ba602498cfdb9d80ffce4f6d7b20d06
snandini <snandini@codeaurora.org>
2017-12-28 14:28:02 -0800

    Release 5.1.1.43F
    Release 5.1.1.43F
    
    Change-Id: I29eab71365a6911fc85d1b4ab6dac832318988b6
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -41,9 +41,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "E"
+#define QWLAN_VERSION_EXTRA            "F"
 #define QWLAN_VERSION_BUILD            43
 
-#define QWLAN_VERSIONSTR               "5.1.1.43E"
+#define QWLAN_VERSIONSTR               "5.1.1.43F"
 
 #endif /* QWLAN_VERSION_H */

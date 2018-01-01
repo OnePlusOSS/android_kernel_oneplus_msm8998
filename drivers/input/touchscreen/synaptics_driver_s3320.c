ad9bcd8fe38614301ba2acce355d35aa7861b5c6
Park Ju Hyung <qkrwngud825@gmail.com>
2018-01-01 18:29:11 +0900

    s3320: turns out we're the only one using int_touch()
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1318,7 +1318,7 @@ bool key_back_pressed;
 bool key_appselect_pressed;
 bool key_home_pressed;
 #endif
-static inline void __int_touch(void)
+static inline void int_touch(void)
 {
 	int ret = -1, i = 0;
 	uint8_t buf[90];
@@ -1345,7 +1345,6 @@ static inline void __int_touch(void)
 	points.z = 0;
 	points.status = 0;
 
-	mutex_lock(&ts->mutexreport);
 #ifdef REPORT_2D_PRESSURE
 	if (ts->support_ft) {
 		ret = i2c_smbus_write_byte_data(ts->client, 0xff, 0x4);
@@ -1355,7 +1354,7 @@ static inline void __int_touch(void)
 
 		if (ret < 0) {
 			TPD_ERR("synaptics_int_touch: i2c_transfer failed\n");
-			goto INT_TOUCH_END;
+			return;
 		}
 		if (points.pressure == 0) {
 			pres_value++;
@@ -1376,7 +1375,7 @@ static inline void __int_touch(void)
 
 	if (ret < 0) {
 		TPD_ERR("snps F12_2D_DATA15: i2c_transfer failed\n");
-		goto INT_TOUCH_END;
+		return;
 	}
 	total_status = (object_attention[1] << 8) | object_attention[0];
 
@@ -1390,14 +1389,14 @@ static inline void __int_touch(void)
 	}
 	if (count_data > 10) {
 		TPD_ERR("count_data is: %d\n", count_data);
-		goto INT_TOUCH_END;
+		return;
 	}
 	ret = synaptics_rmi4_i2c_read_block(ts->client,
 					    F12_2D_DATA_BASE,
 					    count_data * 8 + 1, buf);
 	if (ret < 0) {
 		TPD_ERR("snps F12_2D_DATA_BASE: i2c_transfer failed\n");
-		goto INT_TOUCH_END;
+		return;
 	}
 	for (i = 0; i < count_data; i++) {
 		points.status = buf[i * 8];
@@ -1527,13 +1526,6 @@ static inline void __int_touch(void)
 	if (ts->in_gesture_mode == 1 && ts->is_suspended == 1)
 		gesture_judge(ts);
 #endif
- INT_TOUCH_END:
-	mutex_unlock(&ts->mutexreport);
-}
-
-void int_touch(void)
-{
-	__int_touch();
 }
 
 static char log_count;

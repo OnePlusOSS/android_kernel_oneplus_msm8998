105360628a3b91997311b49679db6cf283a8a4cb
Kiran Kumar Lokere <klokere@codeaurora.org>
2018-01-15 18:18:29 -0800

    qcacld-3.0: Update DTIM value in WMA for modulated DTIM
    Update DTIM value in WMA interface for modulated DTIM
    calculation
    
    Change-Id: I090cb2f33427202edb4e4272e6ac79686fec9cf8
    CRs-Fixed: 2171810

--- core/mac/src/pe/lim/lim_utils.h
+++ core/mac/src/pe/lim/lim_utils.h
@@ -753,6 +753,8 @@ QDF_STATUS lim_p2p_action_cnf(tpAniSirGlobal mac_ctx,
 			uint32_t tx_complete_success);
 void lim_update_caps_info_for_bss(tpAniSirGlobal mac_ctx,
 			uint16_t *caps, uint16_t bss_caps);
+void lim_send_set_dtim_period(tpAniSirGlobal mac_ctx, uint8_t dtim_period,
+			      tpPESession session);
 
 tSirRetStatus lim_strip_ie(tpAniSirGlobal mac_ctx,
 		uint8_t *addn_ie, uint16_t *addn_ielen,

da237f1149e1566011b1830021e700db20d5964a
Ganesh Kondabattini <ganeshk@codeaurora.org>
2018-01-24 18:22:24 +0530

    qcacld-3.0: Correct the logic to decrement the beacon count in CSA IE
    Beacon template may get updated from many use cases like change in
    protection mode, update in CSA count and etc. Make sure that the driver
    maintains the beacon count which is same as in beacon template, otherwise
    there may be duplicate/missing beacon count due to inconsistency.
    
    Decrement the beacon count value before updating the beacon template.
    
    Change-Id: I0c4552445071dd6ab5322191c04d4f7d32c9a987
    CRs-Fixed: 2170097

--- core/mac/src/pe/lim/lim_process_sme_req_messages.c
+++ core/mac/src/pe/lim/lim_process_sme_req_messages.c
@@ -6073,7 +6073,6 @@ skip_vht:
 	lim_send_chan_switch_action_frame(mac_ctx,
 		session_entry->gLimChannelSwitch.primaryChannel,
 		ch_offset, session_entry);
-	session_entry->gLimChannelSwitch.switchCount--;
 }
 
 /**

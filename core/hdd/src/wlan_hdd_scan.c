2c291f6652b309b64072d7271d7fdb78298ef360
Kabilan Kannan <kabilank@codeaurora.org>
2018-02-28 10:36:54 -0800

    qcacld-3.0: Rate limit DBS scan disable message
    Reduce the DBS scan disable messages to lower rate
    to avoid kernel buffer overrun.
    
    Change-Id: I0f8dca7569454613b03a806d453621e53237d93c
    CRs-Fixed: 2197613

--- core/hdd/src/wlan_hdd_scan.c
+++ core/hdd/src/wlan_hdd_scan.c
@@ -59,6 +59,7 @@
  * Count to ratelimit the HDD logs during Scan and connect
  */
 #define HDD_SCAN_REJECT_RATE_LIMIT 5
+#define HDD_DBS_SCAN_DISABLE_RATE_LIMIT 10
 
 /**
  * enum essid_bcast_type - SSID broadcast type
@@ -638,7 +639,8 @@ static void hdd_update_dbs_scan_ctrl_ext_flag(hdd_context_t *hdd_ctx,
 	}
 	if (!(hdd_ctx->is_dbs_scan_duty_cycle_enabled)) {
 		scan_dbs_policy = SME_SCAN_DBS_POLICY_IGNORE_DUTY;
-		hdd_info("DBS scan duty cycle is disabled");
+		hdd_info_ratelimited(HDD_DBS_SCAN_DISABLE_RATE_LIMIT,
+				     "DBS scan duty cycle is disabled");
 		goto end;
 	}
 

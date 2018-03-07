062e0dad2bafd5cdaa8bf5e143e94fb7e4ea8c88
Padma, Santhosh Kumar <skpadma@codeaurora.org>
2018-03-07 18:09:49 +0530

    qcacld-3.0: Add SAE auth timer
    Add changes related to SAE auth timer to handle SAE authentication.
    Start SAE auth timer of duration LIM_AUTH_SAE_TIMER_MS when driver
    wants to trigger SAE authentication. If SAE authentication is not
    completed in LIM_AUTH_SAE_TIMER_MS, then report failure to
    supplicant.
    
    Change-Id: I65f0cb01faa1194c133013eaea4a5d8554a045d2
    CRs-Fixed: 2029357

--- core/mac/src/pe/lim/lim_process_message_queue.c
+++ core/mac/src/pe/lim/lim_process_message_queue.c
@@ -1658,6 +1658,7 @@ static void lim_process_messages(tpAniSirGlobal mac_ctx, tpSirMsgQ msg)
 	case SIR_LIM_DEAUTH_ACK_TIMEOUT:
 	case SIR_LIM_CONVERT_ACTIVE_CHANNEL_TO_PASSIVE:
 	case SIR_LIM_AUTH_RETRY_TIMEOUT:
+	case SIR_LIM_AUTH_SAE_TIMEOUT:
 		/* These timeout messages are handled by MLM sub module */
 		lim_process_mlm_req_messages(mac_ctx, msg);
 		break;

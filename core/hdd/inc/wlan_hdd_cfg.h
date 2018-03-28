35c2d97abb71466e4d1e7bfccb4984f5cf0361bc
Rajeev Kumar <rajekuma@codeaurora.org>
2018-03-28 10:30:12 -0700

    qcacld-3.0: Enable auto PS timer in hdd config file
    Enable auto PS timer in hdd config such that adrastea
    is never stuck in full power mode.
    
    Change-Id: I2668b7c1ba64b67bf524e449e96ec6d79a2c1676
    CRs-Fixed: 2214124

--- core/hdd/inc/wlan_hdd_cfg.h
+++ core/hdd/inc/wlan_hdd_cfg.h
@@ -10847,7 +10847,7 @@ enum restart_beaconing_on_ch_avoid_rule {
  * gAutoBmpsTimerValue - Set Auto BMPS Timer value
  * @Min: 0
  * @Max: 120
- * @Default: 0
+ * @Default: 5
  *
  * This ini is used to set Auto BMPS Timer value in seconds
  *
@@ -10862,7 +10862,7 @@ enum restart_beaconing_on_ch_avoid_rule {
 #define CFG_AUTO_PS_ENABLE_TIMER_NAME          "gAutoBmpsTimerValue"
 #define CFG_AUTO_PS_ENABLE_TIMER_MIN           (0)
 #define CFG_AUTO_PS_ENABLE_TIMER_MAX           (120)
-#define CFG_AUTO_PS_ENABLE_TIMER_DEFAULT       (0)
+#define CFG_AUTO_PS_ENABLE_TIMER_DEFAULT       (5)
 
 #ifdef WLAN_ICMP_DISABLE_PS
 /*

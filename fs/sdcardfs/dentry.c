f5f379320ddacc9bfd381122f4ab1067942f4557
andersonchen <anderson.chen@oneplus.net>
2018-04-23 18:16:12 +0800

    Add performance improvement.
    Do not drop the dentry.
    
    Change-Id: I5fb9ef8bf7784b13349fe1ad65ea8583023a7ffc

--- fs/sdcardfs/dentry.c
+++ fs/sdcardfs/dentry.c
@@ -65,7 +65,6 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	if ((lower_dentry->d_flags & DCACHE_OP_REVALIDATE)) {
 		err = lower_dentry->d_op->d_revalidate(lower_dentry, flags);
 		if (err == 0) {
-			d_drop(dentry);
 			goto out;
 		}
 	}

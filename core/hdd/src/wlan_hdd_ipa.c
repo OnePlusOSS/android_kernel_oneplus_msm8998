023ab46a0a1ca0bf23c44448a6624b9a5c8fb692
jiad <jiad@codeaurora.org>
2018-04-25 16:03:03 +0800

    qcacld-3.0: Fix potential memory leak in hdd ipa
    Memory leak is detected for struct op_msg_type when unloading
    wlan driver.
    Before hdd_ipa_uc_op_cb is invoked, uc_op_msg->msg is overwritten
    to NULL.
    
    Fix is to free uc_op_work->msg in error path in hdd_ipa_uc_op_cb.
    
    Change-Id: I8b04100b183d1eb0c4c5d0ab02de751e2ca078aa
    CRs-Fixed: 2230013

--- core/hdd/src/wlan_hdd_ipa.c
+++ core/hdd/src/wlan_hdd_ipa.c
@@ -4334,13 +4334,14 @@ static void hdd_ipa_uc_op_cb(struct op_msg_type *op_msg, void *usr_ctxt)
 	struct ol_txrx_pdev_t *pdev = cds_get_context(QDF_MODULE_ID_TXRX);
 	QDF_STATUS status = QDF_STATUS_SUCCESS;
 
-	if (!pdev) {
-		HDD_IPA_LOG(QDF_TRACE_LEVEL_FATAL, "pdev is NULL");
+	if (!op_msg) {
+		HDD_IPA_LOG(QDF_TRACE_LEVEL_ERROR, "INVALID ARG");
 		return;
 	}
 
-	if (!op_msg || !usr_ctxt) {
-		HDD_IPA_LOG(QDF_TRACE_LEVEL_ERROR, "INVALID ARG");
+	if (!pdev) {
+		HDD_IPA_LOG(QDF_TRACE_LEVEL_FATAL, "pdev is NULL");
+		qdf_mem_free(op_msg);
 		return;
 	}
 

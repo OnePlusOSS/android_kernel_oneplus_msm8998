5d3dca9c35ad1d5d2353511e85066023c55348ff
Alok Kumar <alokkuma@codeaurora.org>
2018-05-09 14:39:36 +0530

    qcacld-3.0: Fix race condition in resetting vdev and using it
    Currently, possible null pointer dereference issue may happen while
    accessing osif_dev in cds_get_connectivity_stats_pkt_bitmap during
    tx completion. In cases when vdev has gone down and tx completion is
    received, this will leads to NULL osif_dev.
    
    So, continue to hold the lock till pkt_type_bitmap is extracted and
    validate the adapter in hdd_tx_rx_collect_connectivity_stats_info.
    
    Change-Id: I0dbbb4a35c2111bdb6fd6e292d3ef8987f8fcb4d
    CRs-Fixed: 2234220

--- core/hdd/src/wlan_hdd_tx_rx.c
+++ core/hdd/src/wlan_hdd_tx_rx.c
@@ -579,15 +579,13 @@ void hdd_tx_rx_collect_connectivity_stats_info(struct sk_buff *skb,
 			uint8_t *pkt_type)
 {
 	uint32_t pkt_type_bitmap;
+	int errno;
 	hdd_adapter_t *adapter = NULL;
 
 	adapter = (hdd_adapter_t *)context;
-	if (unlikely(adapter->magic != WLAN_HDD_ADAPTER_MAGIC)) {
-		QDF_TRACE(QDF_MODULE_ID_HDD_DATA, QDF_TRACE_LEVEL_ERROR,
-			  "Magic cookie(%x) for adapter sanity verification is invalid",
-			  adapter->magic);
+	errno = hdd_validate_adapter(adapter);
+	if (errno)
 		return;
-	}
 
 	/* ARP tracking is done already. */
 	pkt_type_bitmap = adapter->pkt_type_bitmap;

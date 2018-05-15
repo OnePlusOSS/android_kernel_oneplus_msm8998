ec2205b59000ece7f3d088a711f55c7dbbdd00c4
nshrivas <nshrivas@codeaurora.org>
2018-05-14 18:22:22 -0700

    Release 5.1.1.56U
    Release 5.1.1.56U
    
    Change-Id: I72c5dd62535ad63638fef9d054fd7edac217e5fd
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "T"
+#define QWLAN_VERSION_EXTRA            "U"
 #define QWLAN_VERSION_BUILD            56
 
-#define QWLAN_VERSIONSTR               "5.1.1.56T"
+#define QWLAN_VERSIONSTR               "5.1.1.56U"
 
 #endif /* QWLAN_VERSION_H */

9a55b4431d85ddd21967a585715c7c8e3e49740b
Kabilan Kannan <kabilank@codeaurora.org>
2018-06-12 17:22:33 -0700

    qcacld-3.0: Validate SAR2 response event handle
    Validate SAR2 response event handle and pass the valid
    pointer for result extraction.
    
    Change-Id: I91982c411c77494662f6d00de6078622a3d548ae
    CRs-Fixed: 2259373

--- core/wma/inc/wma_internal.h
+++ core/wma/inc/wma_internal.h
@@ -1233,13 +1233,13 @@ QDF_STATUS wma_set_led_flashing(tp_wma_handle wma_handle,
 
 /**
  * wma_sar_rsp_evt_handler() -  process sar response event from FW.
- * @handle: wma handle
+ * @scn_handle: scn handle
  * @event: event buffer
  * @len: buffer length
  *
  * Return: 0 for success or error code
  */
-int wma_sar_rsp_evt_handler(void *handle, uint8_t *event, uint32_t len);
+int wma_sar_rsp_evt_handler(ol_scn_t scn_handle, uint8_t *event, uint32_t len);
 
 #ifdef FEATURE_WLAN_CH_AVOID
 int wma_channel_avoid_evt_handler(void *handle, uint8_t *event,

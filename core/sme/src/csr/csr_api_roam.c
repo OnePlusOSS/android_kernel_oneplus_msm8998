448a4740f2948c6298f2ea8b3f61af0cf373aaff
Himanshu Agarwal <himanaga@codeaurora.org>
2018-06-14 17:04:44 +0530

    qcacld-3.0: Remove unused code from driver
    Presently, while sending scan offload request to fw, fw is only notified
    whether the channel list is static or dynamic. Fw is not notified whether
    it is dynamic init, dynamic flush or dynamic update. Also, in HOST
    driver it is not being used anywhere.
    
    Remove the code to mark the channel list as dynamic update, dynamic flush
    or dynamic init. Instead, assign the channel list simply as dynamic.
    
    Change-Id: Iad834f07bb61963f0fbb6227ffcedfd1679d1a9e
    CRs-Fixed: 2260715

--- core/sme/src/csr/csr_api_roam.c
+++ core/sme/src/csr/csr_api_roam.c
@@ -2240,7 +2240,7 @@ csr_fetch_ch_lst_from_received_list(tpAniSirGlobal mac_ctx,
 		ch_lst++;
 	}
 	req_buf->ConnectedNetwork.ChannelCount = num_channels;
-	req_buf->ChannelCacheType = CHANNEL_LIST_DYNAMIC_UPDATE;
+	req_buf->ChannelCacheType = CHANNEL_LIST_DYNAMIC;
 	sme_debug("ChannelCacheType %dChannelCount %d",
 		  req_buf->ChannelCacheType, num_channels);
 }
@@ -19189,21 +19189,7 @@ csr_fetch_ch_lst_from_occupied_lst(tpAniSirGlobal mac_ctx,
 		ch_lst++;
 	}
 	req_buf->ConnectedNetwork.ChannelCount = num_channels;
-	/*
-	 * If the profile changes as to what it was earlier, inform the FW
-	 * through FLUSH as ChannelCacheType in which case, the FW will flush
-	 * the occupied channels for the earlier profile and try to learn them
-	 * afresh
-	 */
-	if (reason == REASON_FLUSH_CHANNEL_LIST)
-		req_buf->ChannelCacheType = CHANNEL_LIST_DYNAMIC_FLUSH;
-	else {
-		if (csr_neighbor_roam_is_new_connected_profile(mac_ctx,
-							       session_id))
-			req_buf->ChannelCacheType = CHANNEL_LIST_DYNAMIC_INIT;
-		else
-			req_buf->ChannelCacheType = CHANNEL_LIST_DYNAMIC_UPDATE;
-	}
+	req_buf->ChannelCacheType = CHANNEL_LIST_DYNAMIC;
 	sme_debug("ChannelCacheType %dChannelCount %d",
 		  req_buf->ChannelCacheType, num_channels);
 }
@@ -19307,7 +19293,7 @@ csr_fetch_valid_ch_lst(tpAniSirGlobal mac_ctx,
 	}
 	req_buf->ValidChannelCount = num_channels;
 
-	req_buf->ChannelCacheType = CHANNEL_LIST_DYNAMIC_UPDATE;
+	req_buf->ChannelCacheType = CHANNEL_LIST_DYNAMIC;
 	req_buf->ConnectedNetwork.ChannelCount = num_channels;
 	sme_debug("ChannelCacheType %dChannelCount %d",
 		  req_buf->ChannelCacheType, num_channels);

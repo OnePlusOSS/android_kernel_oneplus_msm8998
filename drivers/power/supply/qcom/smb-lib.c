fa9a7667dc4778e1f040a26c09b6e032dacce252
yangfangbiao <yangfangbiao@oneplus.com>
2018-06-15 12:28:59 +0800

    Improve dash charging
    Check dash when charger type is FLOAT.
    
    Change-Id: I9eb0b711581ae4006b02e22838c47edb36724b2a

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -4051,12 +4051,9 @@ static void smblib_handle_apsd_done(struct smb_charger *chg, bool rising)
 		current_limit_ua = DEFAULT_CDP_MA*1000;
 	else if ((apsd_result->bit) == DCP_CHARGER_BIT)
 		current_limit_ua = DEFAULT_DCP_MA*1000;
-	else if ((apsd_result->bit) == FLOAT_CHARGER_BIT) {
-		if (chg->usb_type_redet_done)
-			current_limit_ua = DEFAULT_DCP_MA*1000;
-		else
-			current_limit_ua = DEFAULT_SDP_MA*1000;
-	} else if ((apsd_result->bit) == OCP_CHARGER_BIT)
+	else if ((apsd_result->bit) == FLOAT_CHARGER_BIT)
+		current_limit_ua = DEFAULT_DCP_MA*1000;
+	else if ((apsd_result->bit) == OCP_CHARGER_BIT)
 		current_limit_ua = DEFAULT_DCP_MA*1000;
 
 	if (chg->is_aging_test)
@@ -4075,7 +4072,8 @@ static void smblib_handle_apsd_done(struct smb_charger *chg, bool rising)
 	pr_info("apsd done,current_now=%d\n",
 		(get_prop_batt_current_now(chg) / 1000));
 	if (apsd_result->bit == DCP_CHARGER_BIT
-		|| apsd_result->bit == OCP_CHARGER_BIT) {
+		|| apsd_result->bit == OCP_CHARGER_BIT
+		|| apsd_result->bit == FLOAT_CHARGER_BIT) {
 		schedule_delayed_work(&chg->check_switch_dash_work,
 					msecs_to_jiffies(500));
 	} else {
@@ -5247,8 +5245,7 @@ static void op_check_allow_switch_dash_work(struct work_struct *work)
 
 	apsd_result = smblib_get_apsd_result(chg);
 	if (((apsd_result->bit != SDP_CHARGER_BIT
-		&& apsd_result->bit != CDP_CHARGER_BIT
-		&& apsd_result->bit != FLOAT_CHARGER_BIT)
+		&& apsd_result->bit != CDP_CHARGER_BIT)
 		&& apsd_result->bit)
 		|| chg->non_std_chg_present)
 		switch_fast_chg(chg);

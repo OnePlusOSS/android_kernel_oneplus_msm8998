d79e903986f8d984e99fc61e8664d33068fa5128
Nachiket Kukade <nkukade@codeaurora.org>
2018-06-18 15:52:36 +0530

    qcacld-3.0: Add upper limit check on Listen Interval configuration
    User can override the Listen Interval value set during association
    by using wifi config set vendor command. Since there is no limit
    check in host, a very high value causes an assert in the Firmware.
    
    Add an upper limit check on the user Listen Interval value.
    
    Change-Id: I8128ccbb875adf57c95a15d0391fb442d3dbbbc3
    CRs-Fixed: 2256334

--- core/hdd/src/wlan_hdd_cfg80211.c
+++ core/hdd/src/wlan_hdd_cfg80211.c
@@ -5732,6 +5732,12 @@ __wlan_hdd_cfg80211_wifi_configuration_set(struct wiphy *wiphy,
 		override_li = nla_get_u32(
 			tb[QCA_WLAN_VENDOR_ATTR_CONFIG_LISTEN_INTERVAL]);
 
+		if (override_li > CFG_ENABLE_DYNAMIC_DTIM_MAX) {
+			hdd_err_ratelimited(HDD_NL_ERR_RATE_LIMIT,
+				"Invalid value for listen interval - %d",
+				override_li);
+			return -EINVAL;
+		}
 		status = sme_override_listen_interval(hdd_ctx->hHal,
 						      adapter->sessionId,
 						      override_li);

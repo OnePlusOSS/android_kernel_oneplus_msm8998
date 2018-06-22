6c831c1f3a47b7edb909e1033385b960122a4776
nshrivas <nshrivas@codeaurora.org>
2018-06-21 20:13:57 -0700

    Release 5.1.1.60X
    Release 5.1.1.60X
    
    Change-Id: Iabb773b17d63ae2558558d1f5ad3bf9488c04180
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "W"
+#define QWLAN_VERSION_EXTRA            "X"
 #define QWLAN_VERSION_BUILD            60
 
-#define QWLAN_VERSIONSTR               "5.1.1.60W"
+#define QWLAN_VERSIONSTR               "5.1.1.60X"
 
 #endif /* QWLAN_VERSION_H */

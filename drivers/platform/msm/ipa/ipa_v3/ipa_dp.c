b34f7b82d8f1f29e4590a7fbff6b6bf4d6baa507
Sultan Alsawaf <sultan@kerneltoast.com>
2018-06-30 12:04:40 +0900

    msm: ipa3: Fix redundant wakelock releases in error paths
    In the error paths here, these functions are re-scheduled when they fail;
    in other words, these functions are run repeatedly until they succeed.
    
    Since the ipa3 wakelock is released before checking for errors, it can be
    released more than once when there is an error and the function that errors
    out needs to be re-run.
    
    To fix this, only release the ipa3 wakelock upon success in these functions.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -249,7 +249,6 @@ static void ipa3_tx_switch_to_intr_mode(struct ipa3_sys_context *sys)
 
 	if (ipa3_ctx->transport_prototype == IPA_TRANSPORT_TYPE_GSI) {
 		atomic_set(&sys->curr_polling_state, 0);
-		ipa3_dec_release_wakelock();
 		ret = gsi_config_channel_mode(sys->ep->gsi_chan_hdl,
 			GSI_CHAN_MODE_CALLBACK);
 		if (ret != GSI_STATUS_SUCCESS) {
@@ -277,8 +276,8 @@ static void ipa3_tx_switch_to_intr_mode(struct ipa3_sys_context *sys)
 		}
 		atomic_set(&sys->curr_polling_state, 0);
 		ipa3_handle_tx_core(sys, true, false);
-		ipa3_dec_release_wakelock();
 	}
+	ipa3_dec_release_wakelock();
 	return;
 
 fail:
@@ -1049,7 +1048,6 @@ static void ipa3_rx_switch_to_intr_mode(struct ipa3_sys_context *sys)
 			goto fail;
 		}
 		atomic_set(&sys->curr_polling_state, 0);
-		ipa3_dec_release_wakelock();
 		ret = gsi_config_channel_mode(sys->ep->gsi_chan_hdl,
 			GSI_CHAN_MODE_CALLBACK);
 		if (ret != GSI_STATUS_SUCCESS) {
@@ -1086,8 +1084,8 @@ static void ipa3_rx_switch_to_intr_mode(struct ipa3_sys_context *sys)
 		}
 		atomic_set(&sys->curr_polling_state, 0);
 		ipa3_handle_rx_core(sys, true, false);
-		ipa3_dec_release_wakelock();
 	}
+	ipa3_dec_release_wakelock();
 	return;
 
 fail:

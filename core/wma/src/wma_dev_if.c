1fe541680e3e153526328e2f029767b86ce38655
Abhinav Kumar <abhikuma@codeaurora.org>
2018-08-08 12:45:15 +0530

    qcacld-3.0: Possible mem leak while handling WMA_DEL_STA_SELF_REQ
    In wma_vdev_resp_timer(), while handling WMA_DEL_STA_SELF_REQ if
    wma_crash_on_fw_timeout() is true, then wma_vdev_resp_timer initiate
    SSR and memset iface structure without freeing iface->del_staself_req.
    This results mem leak in wma_vdev_resp_timer().
    
    Free all the dynamic memory from iface structure in wma_vdev_deinit()
    instead of handling them separately.
    
    Change-Id: I7b16ddc9dfb70638c6f895bd97cd9c106bfad595
    CRs-Fixed: 2293099

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -457,23 +457,6 @@ static void wma_vdev_detach_callback(void *ctx)
 			qdf_mem_free(req_msg);
 		}
 	}
-	if (iface->addBssStaContext)
-		qdf_mem_free(iface->addBssStaContext);
-
-
-	if (iface->staKeyParams)
-		qdf_mem_free(iface->staKeyParams);
-
-	if (iface->stats_rsp)
-		qdf_mem_free(iface->stats_rsp);
-
-	if (iface->roam_scan_stats_req) {
-		struct sir_roam_scan_stats *roam_scan_stats_req =
-						iface->roam_scan_stats_req;
-
-		iface->roam_scan_stats_req = NULL;
-		qdf_mem_free(roam_scan_stats_req);
-	}
 
 	wma_vdev_deinit(iface);
 	qdf_mem_zero(iface, sizeof(*iface));
@@ -596,6 +579,7 @@ static QDF_STATUS wma_handle_vdev_detach(tp_wma_handle wma_handle,
 		WMA_LOGE("%s: Failed to fill vdev request for vdev_id %d",
 			 __func__, vdev_id);
 		status = QDF_STATUS_E_NOMEM;
+		iface->del_staself_req = NULL;
 		goto out;
 	}
 
@@ -621,11 +605,6 @@ out:
 	WMA_LOGE("Call txrx detach callback for vdev %d, generate_rsp %u",
 		vdev_id, generate_rsp);
 	wma_ol_txrx_vdev_detach(wma_handle, vdev_id);
-
-	if (iface->addBssStaContext)
-		qdf_mem_free(iface->addBssStaContext);
-	if (iface->staKeyParams)
-		qdf_mem_free(iface->staKeyParams);
 
 	wma_vdev_deinit(iface);
 	qdf_mem_zero(iface, sizeof(*iface));
@@ -3463,16 +3442,7 @@ void wma_vdev_resp_timer(void *data)
 			iface->del_staself_req = NULL;
 		} else {
 			wma_send_del_sta_self_resp(iface->del_staself_req);
-		}
-
-		if (iface->addBssStaContext) {
-			qdf_mem_free(iface->addBssStaContext);
-			iface->addBssStaContext = NULL;
-		}
-
-		if (iface->staKeyParams) {
-			qdf_mem_free(iface->staKeyParams);
-			iface->staKeyParams = NULL;
+			iface->del_staself_req = NULL;
 		}
 
 		wma_vdev_deinit(iface);

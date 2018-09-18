1ea95e297e6c505ad312c792358964e2652f5b3d
Alok Kumar <alokkuma@codeaurora.org>
2018-09-18 14:24:14 +0530

    qcacld-3.0: Unpause the Priority Queue for deficient pools
    Priority Queue gets paused due to descriptor deficiency which
    needs to be unpaused after distributing descriptors from a
    source pool to deficient pool.
    
    Unpause the Priority Queue of deficient pool when avail->desc
    count is more than start_th.
    
    Change-Id: Ia91fdd0ac39bf8a4ac42dafa42d346993bf6a38e
    CRs-Fixed: 2318031

--- core/hdd/src/wlan_hdd_tx_rx.c
+++ core/hdd/src/wlan_hdd_tx_rx.c
@@ -2054,6 +2054,7 @@ const char *hdd_reason_type_to_string(enum netif_reason_type reason)
 	CASE_RETURN_STRING(WLAN_VDEV_STOP);
 	CASE_RETURN_STRING(WLAN_PEER_UNAUTHORISED);
 	CASE_RETURN_STRING(WLAN_THERMAL_MITIGATION);
+	CASE_RETURN_STRING(WLAN_DATA_FLOW_CONTROL_PRIORITY);
 	default:
 		return "Invalid";
 	}
@@ -2079,6 +2080,8 @@ const char *hdd_action_type_to_string(enum netif_action_type action)
 	CASE_RETURN_STRING(WLAN_START_ALL_NETIF_QUEUE_N_CARRIER);
 	CASE_RETURN_STRING(WLAN_NETIF_CARRIER_ON);
 	CASE_RETURN_STRING(WLAN_NETIF_CARRIER_OFF);
+	CASE_RETURN_STRING(WLAN_NETIF_PRIORITY_QUEUE_ON);
+	CASE_RETURN_STRING(WLAN_NETIF_PRIORITY_QUEUE_OFF);
 	default:
 		return "Invalid";
 	}
@@ -2096,11 +2099,13 @@ static void wlan_hdd_update_queue_oper_stats(hdd_adapter_t *adapter,
 	switch (action) {
 	case WLAN_STOP_ALL_NETIF_QUEUE:
 	case WLAN_STOP_ALL_NETIF_QUEUE_N_CARRIER:
+	case WLAN_NETIF_PRIORITY_QUEUE_OFF:
 		adapter->queue_oper_stats[reason].pause_count++;
 		break;
 	case WLAN_START_ALL_NETIF_QUEUE:
 	case WLAN_WAKE_ALL_NETIF_QUEUE:
 	case WLAN_START_ALL_NETIF_QUEUE_N_CARRIER:
+	case WLAN_NETIF_PRIORITY_QUEUE_ON:
 		adapter->queue_oper_stats[reason].unpause_count++;
 		break;
 	default:

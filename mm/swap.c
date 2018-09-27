713bfe61ccd37bd852599a8200e1580fb5cc25e4
kdrag0n <dragon@khronodragon.com>
2018-09-26 17:21:12 -0700

    mm: swap: swap pages one at a time
    According to Google, this is optimal.
    "By default, the Linux kernel swaps in 8 pages of memory at a time. When
    using ZRAM, the incremental cost of reading 1 page at a time is
    negligible and may help in case the device is under extreme memory
    pressure."
    
    Source: https://source.android.com/devices/tech/perf/low-ram
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- mm/swap.c
+++ mm/swap.c
@@ -40,7 +40,7 @@
 #include <trace/events/pagemap.h>
 
 /* How many pages do we try to swap or page in/out together? */
-int page_cluster;
+int page_cluster = 0;
 
 static DEFINE_PER_CPU(struct pagevec, lru_add_pvec);
 static DEFINE_PER_CPU(struct pagevec, lru_rotate_pvecs);
@@ -1154,7 +1154,6 @@ EXPORT_SYMBOL(pagevec_lookup_range_nr_tag);
  */
 void __init swap_setup(void)
 {
-	unsigned long megs = totalram_pages >> (20 - PAGE_SHIFT);
 #ifdef CONFIG_SWAP
 	int i;
 
@@ -1162,11 +1161,6 @@ void __init swap_setup(void)
 		spin_lock_init(&swapper_spaces[i].tree_lock);
 #endif
 
-	/* Use a smaller cluster for small-memory machines */
-	if (megs < 16)
-		page_cluster = 2;
-	else
-		page_cluster = 3;
 	/*
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more

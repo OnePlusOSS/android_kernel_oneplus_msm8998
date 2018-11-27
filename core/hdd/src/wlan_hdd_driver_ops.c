c70abd094eaa3582d798b1d55fefe222bb88dc1b
Sourav Mohapatra <mohapatr@codeaurora.org>
2018-11-27 12:13:55 +0530

    qcacld-3.0: Do not set recovery flag while load
    Presently the driver recovery in progress flag is set while SSR is
    triggered, even during load time. As the flag is reset during the
    reinit path, which does not get executed if driver is in loading
    state, there occurs scenario where the flag is never reset causing the
    failure of driver load.
    
    Check for driver load before setting the flag.
    
    Change-Id: I1d417ad85f60377b418a85f62fd27b81208cb7fe
    CRs-Fixed: 2356440

--- core/hdd/src/wlan_hdd_driver_ops.c
+++ core/hdd/src/wlan_hdd_driver_ops.c
@@ -1388,7 +1388,8 @@ static void wlan_hdd_set_the_pld_uevent(struct pld_uevent_data *uevent)
 	case PLD_FW_DOWN:
 	case PLD_RECOVERY:
 		cds_set_target_ready(false);
-		cds_set_recovery_in_progress(true);
+		if (!cds_is_driver_loading())
+			cds_set_recovery_in_progress(true);
 		break;
 	default:
 		return;

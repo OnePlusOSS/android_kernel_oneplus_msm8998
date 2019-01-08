ba73de63cc80310a3bf26e9690ddd821853a25df
Jingxiang Ge <jge@codeaurora.org>
2019-01-08 14:43:05 +0800

    qcacld-3.0: Fix potential out of bound issue
    Change to avoid potential out of bound issue
    when unsafe channel numbers parameter is invalid.
    
    Change-Id: I0c2cdac844c5fd644fce61d3d69fefcecdb62f19
    CRs-Fixed: 2374986

--- core/sap/src/sap_ch_select.c
+++ core/sap/src/sap_ch_select.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -446,6 +446,9 @@ void sap_update_unsafe_channel_list(ptSapContext pSapCtx)
 				    unsafe_channel_list,
 				     &unsafe_channel_count,
 				     sizeof(unsafe_channel_list));
+
+	unsafe_channel_count = QDF_MIN(unsafe_channel_count,
+				       (uint16_t)NUM_CHANNELS);
 
 	for (i = 0; i < unsafe_channel_count; i++) {
 		for (j = 0; j < NUM_CHANNELS; j++) {

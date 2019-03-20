a126e20c4312f144a2ddce7549b08b3be8017425
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-20 17:06:09 +0900

    bq27541: optimize a bit
     - Mark bq27541_di static
     - Reduce function calls on update_battery_soc_work()
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/power/supply/qcom/bq27541_fuelgauger.c
+++ drivers/power/supply/qcom/bq27541_fuelgauger.c
@@ -414,7 +414,7 @@ static int bq27541_chip_config(struct bq27541_device_info *di)
 	return 0;
 }
 
-struct bq27541_device_info *bq27541_di;
+static struct bq27541_device_info *bq27541_di;
 static struct i2c_client *new_client;
 
 #define TEN_PERCENT                            10
@@ -1032,7 +1032,7 @@ static struct external_battery_gauge bq27541_batt_gauge = {
 #define LOW_BAT_SOC_UPDATE_MS 6000
 #define RESUME_SCHDULE_SOC_UPDATE_WORK_MS 60000
 
-static int is_usb_pluged(void)
+static inline int is_usb_plugged(void)
 {
 	static struct power_supply *psy;
 	union power_supply_propval ret = {0,};
@@ -1058,7 +1058,7 @@ static int is_usb_pluged(void)
 	return (Vbus > 2500) ? true : false;
 }
 
-static bool get_dash_started(void)
+static inline bool is_dash_started(void)
 {
 	if (bq27541_di && bq27541_di->fastchg_started)
 		return bq27541_di->fastchg_started;
@@ -1068,13 +1068,17 @@ static bool get_dash_started(void)
 
 static void update_battery_soc_work(struct work_struct *work)
 {
+	bool plugged, dash;
 	int vbat;
 
-	if (is_usb_pluged() || get_dash_started()) {
+	plugged = is_usb_plugged();
+	dash = is_dash_started();
+
+	if (plugged || dash) {
 		schedule_delayed_work(
 				&bq27541_di->battery_soc_work,
 				msecs_to_jiffies(BATTERY_SOC_UPDATE_MS));
-		if (get_dash_started())
+		if (dash)
 			return;
 		if (bq27541_di->set_smoothing)
 			return;
@@ -1139,7 +1143,7 @@ static void bq_modify_soc_smooth_parameter(struct work_struct *work)
 
 	di = container_of(work, struct bq27541_device_info,
 			modify_soc_smooth_parameter.work);
-	if (get_dash_started())
+	if (is_dash_started())
 		return;
 	if (di->already_modify_smooth)
 		return;

406cc39b36eac30994e4b422eeb3ad6dcba44cf1
Sumalatha Malothu <smalot@codeaurora.org>
2019-04-29 18:05:53 +0530

    update markings as per reference
    Change-Id: I5cad7fa9d624ffe6f076ca6094eca8a387b1bdb8
    Signed-off-by: Sumalatha Malothu <smalot@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/common/msm_camera_io_util.c
+++ drivers/media/platform/msm/camera_v2/common/msm_camera_io_util.c
@@ -1,5 +1,4 @@
-/* Copyright (c) 2011-2014, 2017, 2019 The Linux Foundataion.
- * All rights reserved.
+/* Copyright (c) 2011-2014, 2017, 2019 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
